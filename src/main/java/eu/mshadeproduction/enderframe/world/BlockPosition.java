567a932d9dffe886951f5ce4d2c17dd9f515e89d
RealAlphaUA <realalpha.ua@gmail.com>
2021-03-26 00:00:09 +0100

    add BlockPosition 🎁

---
+++ src/main/java/eu/mshadeproduction/enderframe/world/BlockPosition.java
@@ -0,0 +1,24 @@
+package eu.mshadeproduction.enderframe.world;
+
+public class BlockPosition {
+
+    private int x, y, z;
+
+    public BlockPosition(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public Integer getZ() {
+        return z;
+    }
+}
