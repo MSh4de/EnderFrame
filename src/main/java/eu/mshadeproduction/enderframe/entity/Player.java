3abeea154b45511d8372c4fc762e5db70516341e
RealAlphaUA <realalpha.ua@gmail.com>
2021-03-28 00:04:49 +0100

    add intercat with chunk 🎢

--- src/main/java/eu/mshadeproduction/enderframe/entity/Player.java
+++ src/main/java/eu/mshadeproduction/enderframe/entity/Player.java
@@ -1,4 +1,19 @@
 package eu.mshadeproduction.enderframe.entity;
 
-public interface Player {
+import eu.mshadeproduction.enderframe.world.Chunk;
+
+import java.util.List;
+
+public interface Player extends Entity {
+
+    String getName();
+
+    void loadChunks(List<Chunk> chunks);
+
+    void loadChunk(Chunk chunk);
+
+    void unLoadChunks(List<Chunk> chunks);
+
+    void unLoadChunk(Chunk chunk);
+
 }
