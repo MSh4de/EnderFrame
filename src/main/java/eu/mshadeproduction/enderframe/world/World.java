484bf6e2dc9a88eec9c1c2e938e6e5a17e6e3ca1
RealAlphaUA <realalpha.ua@gmail.com>
2021-03-28 01:03:13 +0100

    update World 🎞

--- src/main/java/eu/mshadeproduction/enderframe/world/World.java
+++ src/main/java/eu/mshadeproduction/enderframe/world/World.java
@@ -1,9 +1,23 @@
 package eu.mshadeproduction.enderframe.world;
 
+import java.util.Queue;
+
 public interface World {
+
+    String getName();
+
+    long getSeed();
 
     void setBlock(int x, int y, int z, Block block);
 
     Block getBlock(int x, int y, int z);
+
+    Chunk getChunk(int x, int z);
+
+    Queue<Chunk> getChunks();
+
+    void setChunkGenerator(ChunkGenerator chunkGenerator);
+
+    ChunkGenerator getChunkGenerator();
 
 }
