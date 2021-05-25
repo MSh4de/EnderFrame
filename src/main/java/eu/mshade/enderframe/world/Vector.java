e1f1beb32a22da155f40ab335c853dcc92ac231d
RealAlphaUA <realalpha.ua@gmail.com>
2021-05-25 20:00:19 +0200

    add Vector 🧩

---
+++ src/main/java/eu/mshade/enderframe/world/Vector.java
@@ -0,0 +1,110 @@
+package eu.mshade.enderframe.world;
+
+import java.util.Objects;
+
+public class Vector implements Cloneable {
+
+    protected double x;
+    protected double y;
+    protected double z;
+
+
+    public Vector() {
+        this(0, 0, 0);
+    }
+
+    public Vector(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Vector(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Vector(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public Vector setX(double x) {
+        this.x = x;
+        return this;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public Vector setY(double y) {
+        this.y = y;
+        return this;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public Vector setZ(double z) {
+        this.z = z;
+        return this;
+    }
+
+    public Vector add(Vector vec) {
+        x += vec.x;
+        y += vec.y;
+        z += vec.z;
+        return this;
+    }
+
+    public Vector subtract(Vector vec) {
+        x -= vec.x;
+        y -= vec.y;
+        z -= vec.z;
+        return this;
+    }
+
+    public Vector multiply(Vector vec) {
+        x *= vec.x;
+        y *= vec.y;
+        z *= vec.z;
+        return this;
+    }
+
+    public Vector divide(Vector vec) {
+        x /= vec.x;
+        y /= vec.y;
+        z /= vec.z;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Vector)) return false;
+        Vector vector = (Vector) o;
+        return Double.compare(vector.x, x) == 0 && Double.compare(vector.y, y) == 0 && Double.compare(vector.z, z) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, z);
+    }
+
+    @Override
+    public String toString() {
+        return "Vector{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
+    }
+}
