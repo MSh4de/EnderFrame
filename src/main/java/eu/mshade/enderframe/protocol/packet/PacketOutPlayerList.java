bb908b406b7be55880b81c0db45d211498178ce9
RealAlphaUA <realalpha.ua@gmail.com>
2021-07-19 15:19:12 +0200

    add PacketOutPlayerList 🛷

---
+++ src/main/java/eu/mshade/enderframe/protocol/packet/PacketOutPlayerList.java
@@ -0,0 +1,21 @@
+package eu.mshade.enderframe.protocol.packet;
+
+import eu.mshade.enderframe.mojang.chat.TextComponent;
+import eu.mshade.enderframe.protocol.ByteMessage;
+import eu.mshade.enderframe.protocol.PacketOut;
+
+public class PacketOutPlayerList extends PacketOut {
+
+    private String header, footer;
+
+    public PacketOutPlayerList(String header, String footer) {
+        this.header = header;
+        this.footer = footer;
+    }
+
+    @Override
+    public void serialize(ByteMessage byteMessage) {
+        byteMessage.writeTextComponent(TextComponent.of(header));
+        byteMessage.writeTextComponent(TextComponent.of(footer));
+    }
+}
