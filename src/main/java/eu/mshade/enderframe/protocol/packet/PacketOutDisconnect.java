117e52073196a605731962392b7be7001b62acd1
RealAlphaUA <realalpha.ua@gmail.com>
2021-07-19 15:21:27 +0200

    add PacketOutDisconnect 🛒

---
+++ src/main/java/eu/mshade/enderframe/protocol/packet/PacketOutDisconnect.java
@@ -0,0 +1,19 @@
+package eu.mshade.enderframe.protocol.packet;
+
+import eu.mshade.enderframe.mojang.chat.TextComponent;
+import eu.mshade.enderframe.protocol.ByteMessage;
+import eu.mshade.enderframe.protocol.PacketOut;
+
+public class PacketOutDisconnect extends PacketOut {
+
+    private TextComponent textComponent;
+
+    public PacketOutDisconnect(TextComponent textComponent) {
+        this.textComponent = textComponent;
+    }
+
+    @Override
+    public void serialize(ByteMessage byteMessage) {
+        byteMessage.writeTextComponent(textComponent);
+    }
+}
