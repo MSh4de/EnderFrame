a9ffe4684a17849c02a15517ca50acaf2f2322c0
RealAlphaUA <realalpha.ua@gmail.com>
2021-07-19 15:39:20 +0200

    add MetadataType 🧱

---
+++ src/main/java/eu/mshade/enderframe/metadata/MetadataType.java
@@ -0,0 +1,18 @@
+package eu.mshade.enderframe.metadata;
+
+public enum MetadataType {
+
+    BYTE,
+    VAR_INT,
+    FLOAT,
+    STRING,
+    CHAT,
+    OPT_CHAT,
+    SLOT,
+    BOOLEAN,
+    ROTATION,
+    POSITION,
+    OPT_POSITION,
+    DIRECTION,
+    OPT_UUID,
+}
