b1e7fa4cc8366bc8fbb7163ad6b873c97b42b8fc
RealAlphaUA <realalpha.ua@gmail.com>
2021-07-19 15:17:26 +0200

    add PacketPingListener 🎍

---
+++ src/main/java/eu/mshade/enderframe/protocol/temp/listener/PacketPingListener.java
@@ -0,0 +1,23 @@
+package eu.mshade.enderframe.protocol.temp.listener;
+
+import eu.mshade.enderframe.EnderFrame;
+import eu.mshade.enderframe.event.server.ServerPingEvent;
+import eu.mshade.enderframe.protocol.temp.packet.PacketInPing;
+import eu.mshade.mwork.event.EventContainer;
+import eu.mshade.mwork.event.EventListener;
+
+public class PacketPingListener implements EventListener<PacketInPing> {
+
+    @Override
+    public void onEvent(PacketInPing event, EventContainer eventContainer) {
+        EnderFrame.get().getPacketEventBus().publish(new ServerPingEvent(event.getPayload()), eventContainer);
+    }
+
+    /*
+    @Override
+    public void handle(PacketInPing packet, DispatcherContainer dispatcherContainer) {
+        EnderFrame.get().getPacketEventBus().dispatch(new ServerPingEvent(packet.getPayload()), dispatcherContainer);
+    }
+
+     */
+}
