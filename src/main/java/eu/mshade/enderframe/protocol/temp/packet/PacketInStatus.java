38bccdd2b46c0700cdf11091eb90a978a7bfb176
RealAlphaUA <realalpha.ua@gmail.com>
2021-07-19 15:16:17 +0200

    add PacketInStatus 🎗

---
+++ src/main/java/eu/mshade/enderframe/protocol/temp/packet/PacketInStatus.java
@@ -0,0 +1,11 @@
+package eu.mshade.enderframe.protocol.temp.packet;
+
+import eu.mshade.enderframe.protocol.ByteMessage;
+import eu.mshade.enderframe.protocol.PacketIn;
+
+public class PacketInStatus extends PacketIn {
+    @Override
+    public void deserialize(ByteMessage byteMessage) {
+
+    }
+}
