f2b3ee811fa829b8f8010cc30d3c43eada008890
RealAlphaUA <realalpha.ua@gmail.com>
2021-07-19 15:20:44 +0200

    add PacketOutPlayerAbilities 🛶

---
+++ src/main/java/eu/mshade/enderframe/protocol/packet/PacketOutPlayerAbilities.java
@@ -0,0 +1,37 @@
+package eu.mshade.enderframe.protocol.packet;
+
+import eu.mshade.enderframe.protocol.ByteMessage;
+import eu.mshade.enderframe.protocol.PacketOut;
+
+public class PacketOutPlayerAbilities extends PacketOut {
+
+    private boolean invulnerable;
+    private boolean flying;
+    private boolean allowFlying;
+    private boolean instantBreak;
+    private float flyingSpeed;
+    private float walkSpeed;
+
+    public PacketOutPlayerAbilities(boolean invulnerable, boolean flying, boolean allowFlying, boolean instantBreak, float flyingSpeed, float walkSpeed) {
+        this.invulnerable = invulnerable;
+        this.flying = flying;
+        this.allowFlying = allowFlying;
+        this.instantBreak = instantBreak;
+        this.flyingSpeed = flyingSpeed;
+        this.walkSpeed = walkSpeed;
+    }
+
+    @Override
+    public void serialize(ByteMessage byteMessage) {
+        byte b = 0;
+
+        if (this.invulnerable) b |= 1;
+        if (this.flying)  b |= 2;
+        if (this.allowFlying) b |= 4;
+        if (this.instantBreak) b |= 8;
+
+        byteMessage.writeByte(b);
+        byteMessage.writeFloat(this.flyingSpeed);
+        byteMessage.writeFloat(this.walkSpeed);
+    }
+}
