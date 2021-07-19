1404fb2b8a7e60a55253e242b417ae87bc37c131
RealAlphaUA <realalpha.ua@gmail.com>
2021-07-19 15:36:44 +0200

    add TextClickEventType 🎨

---
+++ src/main/java/eu/mshade/enderframe/mojang/chat/TextClickEventType.java
@@ -0,0 +1,17 @@
+package eu.mshade.enderframe.mojang.chat;
+
+public enum TextClickEventType {
+
+    OPEN_URL,
+
+    OPEN_FILE,
+
+    RUN_COMMAND,
+
+    SUGGEST_COMMAND,
+
+    CHANGE_PAGE,
+
+    COPY_TO_CLIPBOARD
+
+}
