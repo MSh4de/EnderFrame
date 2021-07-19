ab56c4e772d391c621a3731e4df10de5b8b2a5e0
RealAlphaUA <realalpha.ua@gmail.com>
2021-07-19 15:24:23 +0200

    add MinecraftEncryption 🎿

---
+++ src/main/java/eu/mshade/enderframe/protocol/MinecraftEncryption.java
@@ -0,0 +1,63 @@
+package eu.mshade.enderframe.protocol;
+
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.*;
+
+public class MinecraftEncryption {
+
+    private KeyPair keyPair;
+
+    public MinecraftEncryption() {
+        try {
+            KeyPairGenerator keypairgenerator = KeyPairGenerator.getInstance("RSA");
+            keypairgenerator.initialize(1024);
+            keyPair = keypairgenerator.generateKeyPair();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public KeyPair getKeyPair() {
+        return keyPair;
+    }
+
+    public String getHashServerId(String s, PublicKey publickey, SecretKey secretkey) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        MessageDigest messagedigest = MessageDigest.getInstance("SHA-1");
+        messagedigest.update(s.getBytes("ISO_8859_1"));
+        messagedigest.update(secretkey.getEncoded());
+        messagedigest.update(publickey.getEncoded());
+        return new BigInteger(messagedigest.digest()).toString(16);
+    }
+
+    public SecretKey getSecretKey(byte[] secretKeyEncrypted) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        Cipher cipher = Cipher.getInstance(this.keyPair.getPrivate().getAlgorithm());
+        cipher.init(2, this.keyPair.getPrivate());
+        return new SecretKeySpec(cipher.doFinal(secretKeyEncrypted), "AES");
+    }
+
+    public static Cipher getCipher(int i, SecretKey secretKey){
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CFB8/NoPadding");
+            cipher.init(i, secretKey, new IvParameterSpec(secretKey.getEncoded()));
+            return cipher;
+        } catch (GeneralSecurityException generalsecurityexception) {
+            throw new RuntimeException(generalsecurityexception);
+        }
+    }
+
+    public static SecretKey createNewSharedKey() {
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+            keyGenerator.init(128);
+            return keyGenerator.generateKey();
+        } catch (NoSuchAlgorithmException var1) {
+            throw new Error(var1);
+        }
+    }
+
+
+}
