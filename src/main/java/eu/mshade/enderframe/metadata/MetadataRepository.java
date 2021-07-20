94af9647eb1cce1f8a4e5c650d4bd51460abb190
NewZ_AZ <BenjaminDeveloppement@gmail.com>
2021-07-20 22:59:23 +0200

    Add MetadataRepository

---
+++ src/main/java/eu/mshade/enderframe/metadata/MetadataRepository.java
@@ -0,0 +1,9 @@
+package eu.mshade.enderframe.metadata;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MetadataRepository {
+    private final Map<MetadataMeaning, Integer> metaDataIndex = new HashMap<>();
+    private final Map<>
+}
