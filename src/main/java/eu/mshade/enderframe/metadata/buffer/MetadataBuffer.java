648af150064e0b53665dac7965f5d79c0d46b3df
NewZ_AZ <BenjaminDeveloppement@gmail.com>
2021-07-25 11:36:35 +0200

    Add MetadataBuffer.java

---
+++ src/main/java/eu/mshade/enderframe/metadata/buffer/MetadataBuffer.java
@@ -0,0 +1,17 @@
+package eu.mshade.enderframe.metadata.buffer;
+
+import eu.mshade.enderframe.metadata.MetadataEntry;
+import eu.mshade.enderframe.metadata.MetadataManager;
+import eu.mshade.enderframe.metadata.MetadataRepository;
+import eu.mshade.enderframe.protocol.ByteMessage;
+
+public abstract class MetadataBuffer {
+
+    private final MetadataRepository metadataRepository = new MetadataRepository();
+
+    public abstract void write(ByteMessage byteMessage, MetadataManager metadataManager, MetadataEntry metadataEntry);
+
+    public MetadataRepository getMetadataRepository() {
+        return metadataRepository;
+    }
+}
