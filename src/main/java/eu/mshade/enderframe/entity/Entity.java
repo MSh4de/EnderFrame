055741220bf6d066aa1f963565ad673f27fd92b7
NewZ_AZ <BenjaminDeveloppement@gmail.com>
2021-07-25 11:28:59 +0200

    Updata Entity.java

--- src/main/java/eu/mshade/enderframe/entity/Entity.java
+++ src/main/java/eu/mshade/enderframe/entity/Entity.java
@@ -1,7 +1,10 @@
 package eu.mshade.enderframe.entity;
 
 import eu.mshade.enderframe.world.Location;
+import eu.mshade.enderframe.world.Vector;
+import eu.mshade.enderframe.world.WorldBuffer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface Entity {
@@ -10,5 +13,42 @@ public interface Entity {
 
     Location getLocation();
 
+    Location getLocation(Location location);
+
+    void setVelocity(Vector velocity);
+
+    Vector getVelocity();
+
+    boolean isOnGround();
+
+    WorldBuffer getWorld();
+
+    boolean teleport(Location location);
+
+    boolean teleport(Location location, TeleportCause TeleportCause);
+
+    boolean teleport(Entity destination);
+
+    boolean teleport(Entity destination, TeleportCause teleportCause);
+    
+    int getEntityId();
+
+    void remove();
+
+    boolean isValid();
+
+    //Server getServer();
+
+    UUID getUniqueId();
+
+    EntityType getType();
+
+    void setCustomName(String name);
+
+    String getCustomName();
+
+    void setCustomNameVisible(boolean flag);
+
+    boolean isCustomNameVisible();
 
 }
