4e4ce539186e5449267660aaf4ab39cce079bb51
NewZ_AZ <BenjaminDeveloppement@gmail.com>
2021-07-25 11:34:31 +0200

    Add Pose.java

---
+++ src/main/java/eu/mshade/enderframe/metadata/Pose.java
@@ -0,0 +1,23 @@
+package eu.mshade.enderframe.metadata;
+
+import java.util.Map;
+
+public enum Pose {
+    STANDING(0),
+    FALL_FLYING(1),
+    SLEEPING(2),
+    SWIMMING(3),
+    SPIN_ATTACK(4),
+    SNEAKING(5),
+    DYING(6);
+
+    private final int id;
+
+    Pose(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
