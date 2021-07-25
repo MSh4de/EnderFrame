e502a162e24edcdfeeea82e486645d2320c6e2ea
NewZ_AZ <BenjaminDeveloppement@gmail.com>
2021-07-25 11:30:37 +0200

    Update TempByteMessage.java

--- src/main/java/eu/mshade/enderframe/protocol/temp/TempByteMessage.java
+++ src/main/java/eu/mshade/enderframe/protocol/temp/TempByteMessage.java
@@ -1,5 +1,7 @@
 package eu.mshade.enderframe.protocol.temp;
 
+import eu.mshade.enderframe.entity.EntityType;
+import eu.mshade.enderframe.metadata.MetadataEntry;
 import eu.mshade.enderframe.protocol.ByteMessage;
 import io.netty.buffer.ByteBuf;
 
@@ -7,5 +9,10 @@ public class TempByteMessage extends ByteMessage {
 
     public TempByteMessage(ByteBuf buf) {
         super(buf);
+    }
+
+    @Override
+    public void writeMetadata(EntityType entityType, MetadataEntry metadataEntry) {
+
     }
 }
