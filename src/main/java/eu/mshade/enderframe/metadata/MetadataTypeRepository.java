568784bc3de7f432109a29d47c3cefbbddf185cc
Zentae <yanistahiri560@gmail.com>
2021-08-05 00:34:41 +0200

    Map renaming. :confused:

--- src/main/java/eu/mshade/enderframe/metadata/MetadataTypeRepository.java
+++ src/main/java/eu/mshade/enderframe/metadata/MetadataTypeRepository.java
@@ -4,14 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MetadataTypeRepository {
-    private final Map<MetadataType, Integer> metadataTypeIndex = new HashMap<>();
+
+    private final Map<MetadataType, Integer> metadataIndex = new HashMap<>();
 
     public void registerMetadataIndex(MetadataType type, int index) {
-        this.metadataTypeIndex.put(type,index);
+        this.metadataIndex.put(type, index);
     }
 
     public int getMetadataIndex(MetadataType metadataType){
-        return this.metadataTypeIndex.get(metadataType);
+        return this.metadataIndex.get(metadataType);
     }
-
 }
