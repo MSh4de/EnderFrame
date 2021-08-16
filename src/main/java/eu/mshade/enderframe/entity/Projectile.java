0d4665e55dce64668d47bd1c500d6c080306c7c4
Zentae <yanistahiri560@gmail.com>
2021-08-16 19:20:28 +0200

    add new Projectile interface. 🏴‍☠️

--- src/main/java/eu/mshade/enderframe/entity/Projectile.java
+++ src/main/java/eu/mshade/enderframe/entity/Projectile.java
@@ -1,2 +1,12 @@
-package eu.mshade.enderframe.entity;public class Projectile {
+package eu.mshade.enderframe.entity;
+
+public interface Projectile {
+
+    ProjectileSource getShooter();
+
+    void setShooter(ProjectileSource source);
+
+    boolean doesBounce();
+
+    void setBounce(boolean var1);
 }
