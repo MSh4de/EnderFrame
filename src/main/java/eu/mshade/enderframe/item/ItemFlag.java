d98c0119eb83426a94c6ffaeea474dcf9bcd4447
RealAlphaUA <realalpha.ua@gmail.com>
2021-08-17 23:42:59 +0200

    add ItemFlag 🎠

---
+++ src/main/java/eu/mshade/enderframe/item/ItemFlag.java
@@ -0,0 +1,17 @@
+package eu.mshade.enderframe.item;
+
+public enum ItemFlag {
+
+    HIDE_ENCHANTS,
+
+    HIDE_ATTRIBUTES,
+
+    HIDE_UNBREAKABLE,
+
+    HIDE_DESTROYS,
+
+    HIDE_PLACED_ON,
+
+    HIDE_POTION_EFFECTS;
+
+}
