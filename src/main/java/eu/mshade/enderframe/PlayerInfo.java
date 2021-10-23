62f74ec2f53693789e7db529fd30579e1e394f90
RealAlphaUA <realalpha.ua@gmail.com>
2021-10-23 22:03:53 +0200

    change type EnderFrameSession to Player 🎭

--- src/main/java/eu/mshade/enderframe/PlayerInfo.java
+++ src/main/java/eu/mshade/enderframe/PlayerInfo.java
@@ -1,30 +1,32 @@
 package eu.mshade.enderframe;
 
+import eu.mshade.enderframe.entity.Player;
+
 import java.util.List;
 
 public class PlayerInfo {
 
     private final PlayerInfoType playerInfoType;
-    private final List<EnderFrameSession> enderFrameSessions;
+    private final List<Player> players;
 
-    public PlayerInfo(PlayerInfoType playerInfoType, List<EnderFrameSession> enderFrameSessions) {
+    public PlayerInfo(PlayerInfoType playerInfoType, List<Player> players) {
         this.playerInfoType = playerInfoType;
-        this.enderFrameSessions = enderFrameSessions;
+        this.players = players;
     }
 
     public PlayerInfoType getPlayerInfoType() {
         return playerInfoType;
     }
 
-    public List<EnderFrameSession> getEnderFrameSessions() {
-        return enderFrameSessions;
+    public List<Player> getPlayers() {
+        return players;
     }
 
     @Override
     public String toString() {
         return "PlayerInfo{" +
                 "playerInfoType=" + playerInfoType +
-                ", enderFrameSessions=" + enderFrameSessions +
+                ", enderFrameSessions=" + players +
                 '}';
     }
 }
--- src/main/java/eu/mshade/enderframe/PlayerInfoBuilder.java
+++ src/main/java/eu/mshade/enderframe/PlayerInfoBuilder.java
@@ -1,12 +1,14 @@
 package eu.mshade.enderframe;
 
+import eu.mshade.enderframe.entity.Player;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class PlayerInfoBuilder {
 
     private PlayerInfoType playerInfoType;
-    private List<EnderFrameSession> enderFrameSessions = new ArrayList<>();
+    private List<Player> players = new ArrayList<>();
 
     private PlayerInfoBuilder(PlayerInfoType playerInfoType) {
         this.playerInfoType = playerInfoType;
@@ -16,13 +18,13 @@ public class PlayerInfoBuilder {
         return new PlayerInfoBuilder(playerInfoType);
     }
 
-    public PlayerInfoBuilder withPlayer(EnderFrameSession enderFrameSession){
-        this.enderFrameSessions.add(enderFrameSession);
+    public PlayerInfoBuilder withPlayer(Player player){
+        this.players.add(player);
         return this;
     }
 
     public PlayerInfo build(){
-        return new PlayerInfo(playerInfoType, enderFrameSessions);
+        return new PlayerInfo(playerInfoType, players);
     }
 
 
