b2808ea77276b7042a52a7f0910597bd4cfae81a
RealAlphaUA <realalpha.ua@gmail.com>
2021-10-23 22:06:21 +0200

    adding metthod getEnderFrameSession, getEnderFrameSessionHandler and update constructor 🎐

--- src/main/java/eu/mshade/enderframe/entity/Player.java
+++ src/main/java/eu/mshade/enderframe/entity/Player.java
@@ -1,9 +1,12 @@
 package eu.mshade.enderframe.entity;
 
+import eu.mshade.enderframe.EnderFrameSession;
 import eu.mshade.enderframe.EnderFrameSessionHandler;
 import eu.mshade.enderframe.GameMode;
 import eu.mshade.enderframe.mojang.GameProfile;
 import eu.mshade.enderframe.mojang.SkinParts;
+import eu.mshade.enderframe.mojang.chat.TextComponent;
+import eu.mshade.enderframe.mojang.chat.TextPosition;
 import eu.mshade.enderframe.protocol.ProtocolVersion;
 import eu.mshade.enderframe.world.Location;
 import eu.mshade.enderframe.world.Vector;
@@ -27,14 +30,14 @@ public abstract class Player extends LivingEntity implements ProjectileSource {
     private GameMode gameMode;
     private GameProfile gameProfile;
 
-    public Player(Location location, Vector velocity, int entityId, boolean isFire, boolean isSneaking, boolean isSprinting, boolean isEating, boolean isInvisible, short airTicks, String customName, boolean isCustomNameVisible, boolean isSilent, UUID uuid, float health, int potionEffectColor, boolean isPotionEffectAmbient, byte numberOfArrowInEntity, boolean isAIDisable, EnderFrameSessionHandler enderFrameSessionHandler, SocketAddress socketAddress, ProtocolVersion protocolVersion, SkinParts skinParts, boolean unused, float absorptionHearts, int score, MOptional<String> displayName, GameMode gameMode, GameProfile gameProfile) {
+    public Player(Location location, Vector velocity, int entityId, boolean isFire, boolean isSneaking, boolean isSprinting, boolean isEating, boolean isInvisible, short airTicks, String customName, boolean isCustomNameVisible, boolean isSilent, UUID uuid, float health, int potionEffectColor, boolean isPotionEffectAmbient, byte numberOfArrowInEntity, boolean isAIDisable, EnderFrameSessionHandler enderFrameSessionHandler, boolean unused, float absorptionHearts, int score, MOptional<String> displayName, GameMode gameMode, GameProfile gameProfile) {
         super(location, velocity, entityId, isFire, isSneaking, isSprinting, isEating, isInvisible, airTicks, customName, isCustomNameVisible, isSilent, uuid, EntityType.PLAYER, health, potionEffectColor, isPotionEffectAmbient, numberOfArrowInEntity, isAIDisable);
         this.enderFrameSessionHandler = enderFrameSessionHandler;
         this.name = gameProfile.getName();
-        this.socketAddress = socketAddress;
-        this.protocolVersion = protocolVersion;
+        this.socketAddress = enderFrameSessionHandler.getEnderFrameSession().getSocketAddress();
+        this.protocolVersion = enderFrameSessionHandler.getProtocolVersion();
         this.ping = 0;
-        this.skinParts = skinParts;
+        this.skinParts = SkinParts.fromByte((byte)127);
         this.unused = unused;
         this.absorptionHearts = absorptionHearts;
         this.score = score;
@@ -43,12 +46,12 @@ public abstract class Player extends LivingEntity implements ProjectileSource {
         this.gameProfile = gameProfile;
     }
 
-    public Player(Location location, int entityId, EnderFrameSessionHandler enderFrameSessionHandler, SocketAddress socketAddress, ProtocolVersion protocolVersion, GameMode gameMode, GameProfile gameProfile) {
+    public Player(Location location, int entityId, EnderFrameSessionHandler enderFrameSessionHandler, GameMode gameMode, GameProfile gameProfile) {
         super(location, EntityType.PLAYER, entityId, 20f);
         this.enderFrameSessionHandler = enderFrameSessionHandler;
         this.name = gameProfile.getName();
-        this.socketAddress = socketAddress;
-        this.protocolVersion = protocolVersion;
+        this.socketAddress = enderFrameSessionHandler.getEnderFrameSession().getSocketAddress();
+        this.protocolVersion = enderFrameSessionHandler.getProtocolVersion();
         this.ping = 0;
         this.skinParts = SkinParts.fromByte((byte)127);
         this.unused = false;
@@ -68,6 +71,24 @@ public abstract class Player extends LivingEntity implements ProjectileSource {
     public <T extends Projectile> T launchProjectile(Class<? extends T> projectile, Vector vector) {
         return null;
     }
+
+    public void sendMessage(TextComponent textComponent, TextPosition textPosition){
+        this.getEnderFrameSessionHandler().getEnderFrameSession().sendMessage(textComponent, textPosition);
+    }
+
+    public void sendMessage(TextComponent textComponent){
+        this.getEnderFrameSessionHandler().getEnderFrameSession().sendMessage(textComponent, TextPosition.CHAT);
+    }
+
+    public void sendMessage(String name){
+        this.getEnderFrameSessionHandler().getEnderFrameSession().sendMessage(name);
+    }
+
+
+    public EnderFrameSession getEnderFrameSession(){
+        return enderFrameSessionHandler.getEnderFrameSession();
+    }
+
 
     public EnderFrameSessionHandler getEnderFrameSessionHandler() {
         return enderFrameSessionHandler;
@@ -132,4 +153,6 @@ public abstract class Player extends LivingEntity implements ProjectileSource {
     public void setGameProfile(GameProfile gameProfile) {
         this.gameProfile = gameProfile;
     }
+
+
 }
