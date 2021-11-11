ed6da6783adcc64d016a892c09438d6e8529473e
NewZAZ <ben.tintin.bp@gmail.com>
2021-11-11 17:29:27 +0100

    delete PropertyItemMeta from implements & change type of displayName from String to TextComponent 🎫

--- src/main/java/eu/mshade/enderframe/item/ItemStack.java
+++ src/main/java/eu/mshade/enderframe/item/ItemStack.java
@@ -1,20 +1,20 @@
 package eu.mshade.enderframe.item;
 
-import eu.mshade.enderframe.item.meta.PropertyItemMeta;
+import eu.mshade.enderframe.mojang.chat.TextComponent;
 
 import java.util.*;
 
-public class ItemStack implements PropertyItemMeta, Cloneable {
+public class ItemStack implements Cloneable {
 
     private Material material;
     private int count;
     private int durability;
     private boolean unbreakable;
-    private String displayName;
+    private TextComponent displayName;
     private List<String> lore = new ArrayList<>();
     private final Map<Enchantment, Integer> enchantments = new HashMap<>();
     private final Set<ItemFlag> itemFlags = new HashSet<>();
-    private Set<Material> adventureBlocks = new LinkedHashSet<>();
+    private final Set<Material> adventureBlocks = new LinkedHashSet<>();
 
     public ItemStack(Material material, int count, int durability) {
         this.material = material;
@@ -22,83 +22,71 @@ public class ItemStack implements PropertyItemMeta, Cloneable {
         this.durability = durability;
     }
 
-    @Override
-    public boolean hasDisplayName() {
-        return displayName != null && displayName.isEmpty();
+    public Material getMaterial() {
+        return material;
     }
 
-    @Override
-    public String getDisplayName() {
+    public boolean hasDisplayName() {
+        return displayName != null;
+    }
+
+    public TextComponent getDisplayName() {
         return displayName;
     }
 
-    @Override
-    public void setDisplayName(String displayName) {
+    public void setDisplayName(TextComponent displayName) {
         this.displayName = displayName;
     }
 
-    @Override
     public boolean hasLore() {
         return lore.isEmpty();
     }
 
-    @Override
     public List<String> getLore() {
         return lore;
     }
 
-    @Override
     public void setLore(String... lore) {
         this.lore = List.of(lore);
     }
 
-    @Override
     public boolean hasEnchants() {
         return enchantments.isEmpty();
     }
 
-    @Override
     public boolean hasEnchantment(Enchantment enchantment) {
         return enchantments.containsKey(enchantment);
     }
 
-    @Override
     public int getEnchantmentLevel(Enchantment enchantment) {
         return enchantments.getOrDefault(enchantment, 0);
     }
 
-    @Override
     public Map<Enchantment, Integer> getEnchantments() {
         return enchantments;
     }
 
-    @Override
     public void addEnchantment(Enchantment enchantment, int level) {
         enchantments.put(enchantment, level);
     }
 
-    @Override
     public void removeEnchant(Enchantment enchantment) {
         enchantments.remove(enchantment);
     }
 
-    @Override
     public void addItemFlags(ItemFlag... itemFlags) {
         for (ItemFlag itemFlag : itemFlags) this.itemFlags.add(itemFlag);
 
     }
 
-    @Override
     public void removeItemFlags(ItemFlag... itemFlags) {
         for (ItemFlag itemFlag : itemFlags) this.itemFlags.remove(itemFlag);
     }
 
-    @Override
     public Collection<ItemFlag> getItemFlags() {
         return itemFlags;
     }
 
-    @Override
     public boolean hasItemFlag(ItemFlag itemFlag) {
         return itemFlags.contains(itemFlag);
     }
@@ -130,9 +118,14 @@ public class ItemStack implements PropertyItemMeta, Cloneable {
         return this;
     }
 
+    public void addAdventureBlocks(Material material){
+        adventureBlocks.add(material);
+    }
+
     public Set<Material> getAdventureBlocks() {
         return adventureBlocks;
     }
+
 
     @Override
     protected ItemStack clone() {
