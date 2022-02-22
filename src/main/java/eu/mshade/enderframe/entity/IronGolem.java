ece1a8490cc051ef97fc6de189d0ca5b96ba4b52
RealAlphaUA <realalpha.ua@gmail.com>
2022-02-22 23:12:12 +0100

    rework IronGolem 🎨

--- src/main/java/eu/mshade/enderframe/entity/IronGolem.java
+++ src/main/java/eu/mshade/enderframe/entity/IronGolem.java
@@ -5,15 +5,20 @@ import eu.mshade.enderframe.world.Vector;
 
 import java.util.UUID;
 
-public interface IronGolem extends LivingEntity {
+public abstract class IronGolem extends LivingEntity {
 
-    /*
-    private boolean isPlayerCreated;
+    private boolean playerCreated;
 
-     */
+    public IronGolem(Location beforeLocation, Location location, Vector velocity, int entityId, boolean fire, boolean sneaking, boolean sprinting, boolean eating, boolean invisible, short airTicks, String customName, boolean customNameVisible, boolean silent, boolean invulnerable, UUID uuid, EntityType entityType, float health, int potionEffectColor, boolean potionEffectAmbient, byte numberOfArrowInEntity, boolean ai, boolean playerCreated) {
+        super(beforeLocation, location, velocity, entityId, fire, sneaking, sprinting, eating, invisible, airTicks, customName, customNameVisible, silent, invulnerable, uuid, entityType, health, potionEffectColor, potionEffectAmbient, numberOfArrowInEntity, ai);
+        this.playerCreated = playerCreated;
+    }
 
+    public boolean isPlayerCreated() {
+        return this.playerCreated;
+    }
 
-    boolean isPlayerCreated();
-
-    void setPlayerCreated(boolean isPlayerCreated);
+    public void setPlayerCreated(boolean isPlayerCreated) {
+        this.playerCreated = isPlayerCreated;
+    }
 }
