23b2c3fe41f2f857c5a6baff73e2b3fe5c66d472
RealAlphaUA <realalpha.ua@gmail.com>
2022-02-22 23:11:37 +0100

    rework Item 🎍

--- src/main/java/eu/mshade/enderframe/entity/Item.java
+++ src/main/java/eu/mshade/enderframe/entity/Item.java
@@ -1,13 +1,26 @@
 package eu.mshade.enderframe.entity;
 
+import eu.mshade.enderframe.item.ItemStack;
 import eu.mshade.enderframe.world.Location;
 import eu.mshade.enderframe.world.Vector;
 
 import java.util.Queue;
 import java.util.UUID;
 
-public interface Item extends Entity {
+public abstract class Item extends Entity {
 
+    protected ItemStack itemStack;
 
-    //
+    public Item(Location beforeLocation, Location location, Vector velocity, int entityId, boolean fire, boolean sneaking, boolean sprinting, boolean eating, boolean invisible, short airTicks, String customName, boolean customNameVisible, boolean silent, boolean invulnerable, UUID uuid, EntityType entityType, ItemStack itemStack) {
+        super(beforeLocation, location, velocity, entityId, fire, sneaking, sprinting, eating, invisible, airTicks, customName, customNameVisible, silent, invulnerable, uuid, entityType);
+        this.itemStack = itemStack;
+    }
+
+    public ItemStack getItemStack() {
+        return itemStack;
+    }
+
+    public void setItemStack(ItemStack itemStack) {
+        this.itemStack = itemStack;
+    }
 }
