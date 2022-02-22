629f1e0f8797f0778566e00dfbdf2acd94e49838
RealAlphaUA <realalpha.ua@gmail.com>
2022-02-22 23:03:01 +0100

    rework Spider 🛒

--- src/main/java/eu/mshade/enderframe/entity/Spider.java
+++ src/main/java/eu/mshade/enderframe/entity/Spider.java
@@ -5,12 +5,21 @@ import eu.mshade.enderframe.world.Vector;
 
 import java.util.UUID;
 
-public interface Spider extends LivingEntity {
+public abstract class Spider extends LivingEntity {
 
-    //private boolean isClimbing;
+    protected boolean climbing;
 
-    boolean isClimbing();
+    public Spider(Location beforeLocation, Location location, Vector velocity, int entityId, boolean fire, boolean sneaking, boolean sprinting, boolean eating, boolean invisible, short airTicks, String customName, boolean customNameVisible, boolean silent, boolean invulnerable, UUID uuid, EntityType entityType, float health, int potionEffectColor, boolean potionEffectAmbient, byte numberOfArrowInEntity, boolean ai, boolean climbing) {
+        super(beforeLocation, location, velocity, entityId, fire, sneaking, sprinting, eating, invisible, airTicks, customName, customNameVisible, silent, invulnerable, uuid, entityType, health, potionEffectColor, potionEffectAmbient, numberOfArrowInEntity, ai);
+        this.climbing = climbing;
+    }
 
-    void setClimbing(boolean climbing);
+    public boolean isClimbing(){
+        return this.climbing;
+    }
+
+    public void setClimbing(boolean climbing){
+        this.climbing = climbing;
+    }
 
 }
