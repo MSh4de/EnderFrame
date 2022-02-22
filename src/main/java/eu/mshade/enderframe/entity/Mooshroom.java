0071e6035ffcca0982b73e1f45bd6a8e961e308a
RealAlphaUA <realalpha.ua@gmail.com>
2022-02-22 23:03:42 +0100

    change interface by abstract class 🦺

--- src/main/java/eu/mshade/enderframe/entity/Mooshroom.java
+++ src/main/java/eu/mshade/enderframe/entity/Mooshroom.java
@@ -5,6 +5,9 @@ import eu.mshade.enderframe.world.Vector;
 
 import java.util.UUID;
 
-public interface Mooshroom extends LivingEntity {
+public abstract class Mooshroom extends LivingEntity {
 
+    public Mooshroom(Location beforeLocation, Location location, Vector velocity, int entityId, boolean fire, boolean sneaking, boolean sprinting, boolean eating, boolean invisible, short airTicks, String customName, boolean customNameVisible, boolean silent, boolean invulnerable, UUID uuid, EntityType entityType, float health, int potionEffectColor, boolean potionEffectAmbient, byte numberOfArrowInEntity, boolean ai) {
+        super(beforeLocation, location, velocity, entityId, fire, sneaking, sprinting, eating, invisible, airTicks, customName, customNameVisible, silent, invulnerable, uuid, entityType, health, potionEffectColor, potionEffectAmbient, numberOfArrowInEntity, ai);
+    }
 }
