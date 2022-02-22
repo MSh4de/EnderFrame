0cde052bb8f1d1690a61efc3de3ada6959726bc7
RealAlphaUA <realalpha.ua@gmail.com>
2022-02-22 01:58:39 +0100

    auto calculated unused 🥽

--- src/main/java/eu/mshade/enderframe/mojang/SkinParts.java
+++ src/main/java/eu/mshade/enderframe/mojang/SkinParts.java
@@ -9,9 +9,8 @@ public class SkinParts {
     private boolean leftPantsLeg;
     private boolean rightPantsLeg;
     private boolean hat;
-    private boolean unused;
 
-    public SkinParts(boolean cape, boolean jacket, boolean leftSleeve, boolean rightSleeve, boolean leftPantsLeg, boolean rightPantsLeg, boolean hat, boolean unused) {
+    public SkinParts(boolean cape, boolean jacket, boolean leftSleeve, boolean rightSleeve, boolean leftPantsLeg, boolean rightPantsLeg, boolean hat) {
         this.cape = cape;
         this.jacket = jacket;
         this.leftSleeve = leftSleeve;
@@ -19,10 +18,73 @@ public class SkinParts {
         this.leftPantsLeg = leftPantsLeg;
         this.rightPantsLeg = rightPantsLeg;
         this.hat = hat;
-        this.unused = unused;
     }
 
-    public static SkinParts fromByte(byte b){
+    public boolean isCape() {
+        return cape;
+    }
+
+    public SkinParts setCape(boolean cape) {
+        this.cape = cape;
+        return this;
+    }
+
+    public boolean isJacket() {
+        return jacket;
+    }
+
+    public SkinParts setJacket(boolean jacket) {
+        this.jacket = jacket;
+        return this;
+    }
+
+    public boolean isLeftSleeve() {
+        return leftSleeve;
+    }
+
+    public SkinParts setLeftSleeve(boolean leftSleeve) {
+        this.leftSleeve = leftSleeve;
+        return this;
+    }
+
+    public boolean isRightSleeve() {
+        return rightSleeve;
+    }
+
+    public SkinParts setRightSleeve(boolean rightSleeve) {
+        this.rightSleeve = rightSleeve;
+        return this;
+    }
+
+    public boolean isLeftPantsLeg() {
+        return leftPantsLeg;
+    }
+
+    public SkinParts setLeftPantsLeg(boolean leftPantsLeg) {
+        this.leftPantsLeg = leftPantsLeg;
+        return this;
+    }
+
+    public boolean isRightPantsLeg() {
+        return rightPantsLeg;
+    }
+
+    public SkinParts setRightPantsLeg(boolean rightPantsLeg) {
+        this.rightPantsLeg = rightPantsLeg;
+        return this;
+    }
+
+    public boolean isHat() {
+        return hat;
+    }
+
+    public SkinParts setHat(boolean hat) {
+        this.hat = hat;
+        return this;
+    }
+
+
+    public static SkinParts fromByte(byte b) {
 
         boolean cape = b == (byte) (b | 0x01);
 
@@ -38,12 +100,10 @@ public class SkinParts {
 
         boolean hat = b == (byte) (b | 0x40);
 
-        boolean unused = b == (byte) (b |0x80);
-
-        return new SkinParts(cape,jacket,leftSleeve,rightSleeve,leftPantsLeg,rightPantsLeg,hat,unused);
+        return new SkinParts(cape, jacket, leftSleeve, rightSleeve, leftPantsLeg, rightPantsLeg, hat);
     }
 
-    public byte toByte(){
+    public byte toByte() {
         byte b = 0;
 
         if (cape) b = (byte) (b | 0x01);
@@ -60,7 +120,7 @@ public class SkinParts {
 
         if (hat) b = (byte) (b | 0x40);
 
-        if(unused) b = (byte) (b |0x80);
+        if (cape && jacket && leftSleeve && rightSleeve && leftPantsLeg && rightPantsLeg && hat) b = (byte) (b | 0x80);
 
         return b;
     }
@@ -75,7 +135,6 @@ public class SkinParts {
                 ", leftPantsLeg=" + leftPantsLeg +
                 ", rightPantsLeg=" + rightPantsLeg +
                 ", hat=" + hat +
-                ", unused=" + unused +
                 '}';
     }
 }
