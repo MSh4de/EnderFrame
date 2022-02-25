8b687a8bb5730ff5128418510b19e7baba5ba497
RealAlphaUA <realalpha.ua@gmail.com>
2022-02-26 00:34:57 +0100

    complet class 🎪

--- src/main/java/eu/mshade/enderframe/entity/NonPlayerCharacter.java
+++ src/main/java/eu/mshade/enderframe/entity/NonPlayerCharacter.java
@@ -1,4 +1,20 @@
 package eu.mshade.enderframe.entity;
 
+import eu.mshade.enderframe.GameMode;
+import eu.mshade.enderframe.mojang.GameProfile;
+import eu.mshade.enderframe.mojang.SkinParts;
+import eu.mshade.enderframe.protocol.ProtocolVersion;
+import eu.mshade.enderframe.world.Location;
+import eu.mshade.enderframe.world.Vector;
+
+import java.net.SocketAddress;
+import java.util.Optional;
+import java.util.UUID;
+
 public abstract class NonPlayerCharacter extends Player{
+
+    public NonPlayerCharacter(Location beforeLocation, Location location, Vector velocity, int entityId, boolean fire, boolean sneaking, boolean sprinting, boolean eating, boolean invisible, short airTicks, String customName, boolean customNameVisible, boolean silent, boolean invulnerable, UUID uuid, EntityType entityType, float health, int potionEffectColor, boolean potionEffectAmbient, byte numberOfArrowInEntity, boolean ai, String name, SocketAddress socketAddress, ProtocolVersion protocolVersion, int ping, SkinParts skinParts, float absorptionHearts, int score, Optional<String> playerListName, GameMode gameMode, GameProfile gameProfile) {
+        super(beforeLocation, location, velocity, entityId, fire, sneaking, sprinting, eating, invisible, airTicks, customName, customNameVisible, silent, invulnerable, uuid, entityType, health, potionEffectColor, potionEffectAmbient, numberOfArrowInEntity, ai, name, socketAddress, protocolVersion, ping, skinParts, absorptionHearts, score, playerListName, gameMode, gameProfile);
+    }
+
 }
