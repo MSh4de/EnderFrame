0f57708597a37c2c1ba177a21e036917742b01bf
RealAlphaUA <realalpha.ua@gmail.com>
2022-03-01 00:08:50 +0100

    adding constructor 🦺

--- src/main/java/eu/mshade/enderframe/entity/MagmaCube.java
+++ src/main/java/eu/mshade/enderframe/entity/MagmaCube.java
@@ -7,4 +7,7 @@ import java.util.UUID;
 
 public abstract class MagmaCube extends Slime{
 
+    public MagmaCube(Location beforeLocation, Location location, Vector velocity, int entityId, boolean fire, boolean sneaking, boolean sprinting, boolean eating, boolean invisible, short airTicks, String customName, boolean customNameVisible, boolean silent, boolean invulnerable, UUID uuid, EntityType entityType, float health, int potionEffectColor, boolean potionEffectAmbient, byte numberOfArrowInEntity, boolean ai, byte size) {
+        super(beforeLocation, location, velocity, entityId, fire, sneaking, sprinting, eating, invisible, airTicks, customName, customNameVisible, silent, invulnerable, uuid, entityType, health, potionEffectColor, potionEffectAmbient, numberOfArrowInEntity, ai, size);
+    }
 }
