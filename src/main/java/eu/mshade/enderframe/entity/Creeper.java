04c4fb5155fcb8d368fd2427a61ffada8e8c3d60
RealAlphaUA <realalpha.ua@gmail.com>
2022-03-01 00:09:59 +0100

    remove prefix is 🎪

--- src/main/java/eu/mshade/enderframe/entity/Creeper.java
+++ src/main/java/eu/mshade/enderframe/entity/Creeper.java
@@ -3,18 +3,17 @@ package eu.mshade.enderframe.entity;
 import eu.mshade.enderframe.world.Location;
 import eu.mshade.enderframe.world.Vector;
 
-import java.util.Queue;
 import java.util.UUID;
 
 public abstract class Creeper extends LivingEntity {
 
     protected CreeperState creeperState;
-    protected boolean isPowered;
+    protected boolean powered;
 
-    public Creeper(Location beforeLocation, Location location, Vector velocity, int entityId, boolean fire, boolean sneaking, boolean sprinting, boolean eating, boolean invisible, short airTicks, String customName, boolean customNameVisible, boolean silent, boolean invulnerable, UUID uuid, EntityType entityType, float health, int potionEffectColor, boolean potionEffectAmbient, byte numberOfArrowInEntity, boolean ai, CreeperState creeperState, boolean isPowered) {
+    public Creeper(Location beforeLocation, Location location, Vector velocity, int entityId, boolean fire, boolean sneaking, boolean sprinting, boolean eating, boolean invisible, short airTicks, String customName, boolean customNameVisible, boolean silent, boolean invulnerable, UUID uuid, EntityType entityType, float health, int potionEffectColor, boolean potionEffectAmbient, byte numberOfArrowInEntity, boolean ai, CreeperState creeperState, boolean powered) {
         super(beforeLocation, location, velocity, entityId, fire, sneaking, sprinting, eating, invisible, airTicks, customName, customNameVisible, silent, invulnerable, uuid, entityType, health, potionEffectColor, potionEffectAmbient, numberOfArrowInEntity, ai);
         this.creeperState = creeperState;
-        this.isPowered = isPowered;
+        this.powered = powered;
     }
 
     public CreeperState getCreeperState() {
@@ -26,11 +25,11 @@ public abstract class Creeper extends LivingEntity {
     }
 
     public boolean isPowered() {
-        return isPowered;
+        return powered;
     }
 
     public void setPowered(boolean powered) {
-        isPowered = powered;
+        this.powered = powered;
     }
 
 }
