db038af0714e528e150ade346575f2137b73954d
RealAlphaUA <realalpha.ua@gmail.com>
2022-03-01 00:09:38 +0100

    adding constructor 🎀

--- src/main/java/eu/mshade/enderframe/entity/FurnaceMinecart.java
+++ src/main/java/eu/mshade/enderframe/entity/FurnaceMinecart.java
@@ -1,9 +1,18 @@
 package eu.mshade.enderframe.entity;
 
+import eu.mshade.enderframe.world.Location;
+import eu.mshade.enderframe.world.Vector;
+
+import java.util.UUID;
+
 public abstract class FurnaceMinecart extends Minecart {
 
     protected boolean powered;
 
+    public FurnaceMinecart(Location beforeLocation, Location location, Vector velocity, int entityId, boolean fire, boolean sneaking, boolean sprinting, boolean eating, boolean invisible, short airTicks, String customName, boolean customNameVisible, boolean silent, boolean invulnerable, UUID uuid, EntityType entityType, int shakingPower, int shakingDirection, int blockId, int blockData, int blockYPosition, boolean showBlock, boolean powered) {
+        super(beforeLocation, location, velocity, entityId, fire, sneaking, sprinting, eating, invisible, airTicks, customName, customNameVisible, silent, invulnerable, uuid, entityType, shakingPower, shakingDirection, blockId, blockData, blockYPosition, showBlock);
+        this.powered = powered;
+    }
 
     public boolean isPowered() {
         return powered;
