9699ebf81b07e41a933aab2b366b22b4b61cf519
RealAlphaUA <realalpha.ua@gmail.com>
2022-03-05 00:05:51 +0100

    add method setEntityMetadata and adding method for supportedEntityMetadataType 🎢

--- src/main/java/eu/mshade/enderframe/metadata/EntityMetadataBucket.java
+++ src/main/java/eu/mshade/enderframe/metadata/EntityMetadataBucket.java
@@ -2,23 +2,36 @@ package eu.mshade.enderframe.metadata;
 
 import eu.mshade.enderframe.entity.metadata.InvisibleEntityMetadata;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class EntityMetadataBucket {
 
     private final Map<EntityMetadataType, EntityMetadata<?>> entityMetadataByType = new HashMap<>();
     private final Set<EntityMetadataType> supportedEntityMetadataType = new HashSet<>();
-    
+
 
     public <T> EntityMetadata<T> getEntityMetadata(EntityMetadataType entityMetadataType){
         return (EntityMetadata<T>) this.entityMetadataByType.get(entityMetadataType);
     }
 
+    public void setEntityMetadata(EntityMetadata<?> entityMetadata){
+        if (supportedEntityMetadataType.contains(entityMetadata.getEntityMetadataType())) this.entityMetadataByType.put(entityMetadata.getEntityMetadataType(), entityMetadata);
+    }
+
     public boolean hasEntityMetadata(EntityMetadataType entityMetadataType){
         return this.entityMetadataByType.containsKey(entityMetadataType);
+    }
+
+    public void addSupportedEntityMetadataType(EntityMetadataType... entityMetadataTypes){
+        this.supportedEntityMetadataType.addAll(List.of(entityMetadataTypes));
+    }
+
+    public void removeSupportedEntityMetadataType(EntityMetadataType... entityMetadataTypes){
+        this.supportedEntityMetadataType.removeAll(List.of(entityMetadataTypes));
+    }
+
+    public boolean isSupportEntityMetadataType(EntityMetadataType entityMetadataType){
+        return this.supportedEntityMetadataType.contains(entityMetadataType);
     }
 
 }
