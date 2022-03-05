532e43b2dfc867f25fab2ed1077c817c9324f704
RealAlphaUA <realalpha.ua@gmail.com>
2022-03-05 13:37:50 +0100

    using EntityMetadataBucket 🎨

--- src/main/java/eu/mshade/enderframe/entity/EnderDragon.java
+++ src/main/java/eu/mshade/enderframe/entity/EnderDragon.java
@@ -7,7 +7,12 @@ import java.util.UUID;
 
 public abstract class EnderDragon extends LivingEntity {
 
-    public EnderDragon(Location beforeLocation, Location location, Vector velocity, int entityId, boolean fire, boolean sneaking, boolean sprinting, boolean eating, boolean invisible, short airTicks, String customName, boolean customNameVisible, boolean silent, boolean invulnerable, UUID uuid, EntityType entityType, float health, int potionEffectColor, boolean potionEffectAmbient, byte numberOfArrowInEntity, boolean ai) {
-        super(beforeLocation, location, velocity, entityId, fire, sneaking, sprinting, eating, invisible, airTicks, customName, customNameVisible, silent, invulnerable, uuid, entityType, health, potionEffectColor, potionEffectAmbient, numberOfArrowInEntity, ai);
+
+    public EnderDragon(Location location, Vector velocity, int entityId, UUID uuid) {
+        super(location, velocity, entityId, uuid, EntityType.ENDER_DRAGON);
+    }
+
+    public EnderDragon(Location location, int entityId) {
+        this(location, new Vector(), entityId, UUID.randomUUID());
     }
 }
