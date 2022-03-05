eb0be9a124bdd3ee5b66d15a7c75548c4094cd3b
RealAlphaUA <realalpha.ua@gmail.com>
2022-03-05 13:38:11 +0100

    using EntityMetadataBucket 🎨

--- src/main/java/eu/mshade/enderframe/entity/Chicken.java
+++ src/main/java/eu/mshade/enderframe/entity/Chicken.java
@@ -7,9 +7,15 @@ import java.util.UUID;
 
 public abstract class Chicken extends LivingEntity implements Ageable, Breedable {
 
-    public Chicken(Location beforeLocation, Location location, Vector velocity, int entityId, boolean fire, boolean sneaking, boolean sprinting, boolean eating, boolean invisible, short airTicks, String customName, boolean customNameVisible, boolean silent, boolean invulnerable, UUID uuid, EntityType entityType, float health, int potionEffectColor, boolean potionEffectAmbient, byte numberOfArrowInEntity, boolean ai) {
-        super(beforeLocation, location, velocity, entityId, fire, sneaking, sprinting, eating, invisible, airTicks, customName, customNameVisible, silent, invulnerable, uuid, entityType, health, potionEffectColor, potionEffectAmbient, numberOfArrowInEntity, ai);
+    public Chicken(Location location, Vector velocity, int entityId, UUID uuid) {
+        super(location, velocity, entityId, uuid, EntityType.CHICKEN);
     }
+
+    public Chicken(Location location, int entityId) {
+        this(location, new Vector(), entityId, UUID.randomUUID());
+    }
+
+
 
     /*
     private int age;
