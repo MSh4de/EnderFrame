e0f293817684e2784ab0380f92c625ead21e1561
RealAlphaUA <realalpha.ua@gmail.com>
2022-03-11 00:37:23 +0100

    convert to EntityMetadataBucket 🎁

--- src/main/java/eu/mshade/enderframe/entity/Minecart.java
+++ src/main/java/eu/mshade/enderframe/entity/Minecart.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 
 public abstract class Minecart extends Entity implements Damageable {
 
+    /*
     protected int shakingPower;
     protected int shakingDirection;
     protected int blockId;
@@ -14,61 +15,16 @@ public abstract class Minecart extends Entity implements Damageable {
     protected int blockYPosition;
     protected boolean showBlock;
 
-    public Minecart(Location beforeLocation, Location location, Vector velocity, int entityId, boolean fire, boolean sneaking, boolean sprinting, boolean eating, boolean invisible, short airTicks, String customName, boolean customNameVisible, boolean silent, boolean invulnerable, UUID uuid, EntityType entityType, int shakingPower, int shakingDirection, int blockId, int blockData, int blockYPosition, boolean showBlock) {
-        super(beforeLocation, location, velocity, entityId, fire, sneaking, sprinting, eating, invisible, airTicks, customName, customNameVisible, silent, invulnerable, uuid, entityType);
-        this.shakingPower = shakingPower;
-        this.shakingDirection = shakingDirection;
-        this.blockId = blockId;
-        this.blockData = blockData;
-        this.blockYPosition = blockYPosition;
-        this.showBlock = showBlock;
+     */
+
+
+    public Minecart(Location location, Vector velocity, int entityId, UUID uuid, EntityType entityType) {
+        super(location, velocity, entityId, uuid, entityType);
     }
 
-    public int getShakingPower() {
-        return shakingPower;
+    public Minecart(Location location, int entityId, EntityType entityType) {
+        this(location, new Vector(), entityId, UUID.randomUUID(), entityType);
     }
 
-    public void setShakingPower(int shakingPower) {
-        this.shakingPower = shakingPower;
-    }
 
-    public int getShakingDirection() {
-        return shakingDirection;
-    }
-
-    public void setShakingDirection(int shakingDirection) {
-        this.shakingDirection = shakingDirection;
-    }
-
-    public int getBlockId() {
-        return blockId;
-    }
-
-    public void setBlockId(int blockId) {
-        this.blockId = blockId;
-    }
-
-    public int getBlockData() {
-        return blockData;
-    }
-
-    public void setBlockData(int blockData) {
-        this.blockData = blockData;
-    }
-
-    public int getBlockYPosition() {
-        return blockYPosition;
-    }
-
-    public void setBlockYPosition(int blockYPosition) {
-        this.blockYPosition = blockYPosition;
-    }
-
-    public boolean isShowBlock() {
-        return showBlock;
-    }
-
-    public void setShowBlock(boolean showBlock) {
-        this.showBlock = showBlock;
-    }
 }
