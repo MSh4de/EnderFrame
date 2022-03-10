cc3e318fb4fef969f9467cee0b3713d965f1ccb2
RealAlphaUA <realalpha.ua@gmail.com>
2022-03-11 00:38:01 +0100

    convert to EntityMetadataBucket 🎁

--- src/main/java/eu/mshade/enderframe/entity/Snowman.java
+++ src/main/java/eu/mshade/enderframe/entity/Snowman.java
@@ -7,7 +7,12 @@ import java.util.UUID;
 
 public abstract class Snowman extends LivingEntity {
 
-    public Snowman(Location beforeLocation, Location location, Vector velocity, int entityId, boolean fire, boolean sneaking, boolean sprinting, boolean eating, boolean invisible, short airTicks, String customName, boolean customNameVisible, boolean silent, boolean invulnerable, UUID uuid, EntityType entityType, float health, int potionEffectColor, boolean potionEffectAmbient, byte numberOfArrowInEntity, boolean ai) {
-        super(beforeLocation, location, velocity, entityId, fire, sneaking, sprinting, eating, invisible, airTicks, customName, customNameVisible, silent, invulnerable, uuid, entityType, health, potionEffectColor, potionEffectAmbient, numberOfArrowInEntity, ai);
+
+    public Snowman(Location location, Vector velocity, int entityId, UUID uuid) {
+        super(location, velocity, entityId, uuid, EntityType.SNOWMAN);
+    }
+
+    public Snowman(Location location, int entityId) {
+        super(location, entityId, EntityType.SNOWMAN);
     }
 }
