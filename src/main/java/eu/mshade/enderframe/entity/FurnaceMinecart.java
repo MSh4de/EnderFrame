b5a55638711b63dd07935865d3e3f28a9cbc4396
RealAlphaUA <realalpha.ua@gmail.com>
2022-03-11 00:36:02 +0100

    convert to EntityMetadataBucket 🎁

--- src/main/java/eu/mshade/enderframe/entity/FurnaceMinecart.java
+++ src/main/java/eu/mshade/enderframe/entity/FurnaceMinecart.java
@@ -7,18 +7,18 @@ import java.util.UUID;
 
 public abstract class FurnaceMinecart extends Minecart {
 
+    public FurnaceMinecart(Location location, Vector velocity, int entityId, UUID uuid) {
+        super(location, velocity, entityId, uuid, EntityType.FURNACE_MINECART);
+    }
+
+    public FurnaceMinecart(Location location, int entityId) {
+        super(location, entityId, EntityType.FURNACE_MINECART);
+    }
+
+    /*
     protected boolean powered;
 
-    public FurnaceMinecart(Location beforeLocation, Location location, Vector velocity, int entityId, boolean fire, boolean sneaking, boolean sprinting, boolean eating, boolean invisible, short airTicks, String customName, boolean customNameVisible, boolean silent, boolean invulnerable, UUID uuid, EntityType entityType, int shakingPower, int shakingDirection, int blockId, int blockData, int blockYPosition, boolean showBlock, boolean powered) {
-        super(beforeLocation, location, velocity, entityId, fire, sneaking, sprinting, eating, invisible, airTicks, customName, customNameVisible, silent, invulnerable, uuid, entityType, shakingPower, shakingDirection, blockId, blockData, blockYPosition, showBlock);
-        this.powered = powered;
-    }
+     */
 
-    public boolean isPowered() {
-        return powered;
-    }
 
-    public void setPowered(boolean powered) {
-        this.powered = powered;
-    }
 }
