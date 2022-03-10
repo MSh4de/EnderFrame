fd12e2ad52369b13ba5794cbb9044ab052e91ea1
RealAlphaUA <realalpha.ua@gmail.com>
2022-03-11 00:37:07 +0100

    convert to EntityMetadataBucket 🎁

--- src/main/java/eu/mshade/enderframe/entity/ItemFrame.java
+++ src/main/java/eu/mshade/enderframe/entity/ItemFrame.java
@@ -8,29 +8,18 @@ import java.util.UUID;
 
 public abstract class ItemFrame extends Entity implements Hanging {
 
-
+    /*
     protected byte rotation;
     protected ItemStack itemStack;
 
-    public ItemFrame(Location beforeLocation, Location location, Vector velocity, int entityId, boolean fire, boolean sneaking, boolean sprinting, boolean eating, boolean invisible, short airTicks, String customName, boolean customNameVisible, boolean silent, boolean invulnerable, UUID uuid, EntityType entityType, byte rotation, ItemStack itemStack) {
-        super(beforeLocation, location, velocity, entityId, fire, sneaking, sprinting, eating, invisible, airTicks, customName, customNameVisible, silent, invulnerable, uuid, entityType);
-        this.rotation = rotation;
-        this.itemStack = itemStack;
+     */
+
+    public ItemFrame(Location location, Vector velocity, int entityId, UUID uuid) {
+        super(location, velocity, entityId, uuid, EntityType.ITEM_FRAME);
     }
 
-    public byte getRotation() {
-        return rotation;
+    public ItemFrame(Location location, int entityId) {
+        super(location, entityId, EntityType.ITEM_FRAME);
     }
 
-    public void setRotation(byte rotation) {
-        this.rotation = rotation;
-    }
-
-    public ItemStack getItemStack() {
-        return itemStack;
-    }
-
-    public void setItemStack(ItemStack itemStack) {
-        this.itemStack = itemStack;
-    }
 }
