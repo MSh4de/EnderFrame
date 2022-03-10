007f63c6193484436ddbb3ae36b4984d8402a856
RealAlphaUA <realalpha.ua@gmail.com>
2022-03-11 00:37:18 +0100

    convert to EntityMetadataBucket 🎁

--- src/main/java/eu/mshade/enderframe/entity/MagmaCube.java
+++ src/main/java/eu/mshade/enderframe/entity/MagmaCube.java
@@ -7,7 +7,12 @@ import java.util.UUID;
 
 public abstract class MagmaCube extends Slime{
 
-    public MagmaCube(Location beforeLocation, Location location, Vector velocity, int entityId, boolean fire, boolean sneaking, boolean sprinting, boolean eating, boolean invisible, short airTicks, String customName, boolean customNameVisible, boolean silent, boolean invulnerable, UUID uuid, EntityType entityType, float health, int potionEffectColor, boolean potionEffectAmbient, byte numberOfArrowInEntity, boolean ai, byte size) {
-        super(beforeLocation, location, velocity, entityId, fire, sneaking, sprinting, eating, invisible, airTicks, customName, customNameVisible, silent, invulnerable, uuid, entityType, health, potionEffectColor, potionEffectAmbient, numberOfArrowInEntity, ai, size);
+
+    public MagmaCube(Location location, Vector velocity, int entityId, UUID uuid) {
+        super(location, velocity, entityId, uuid, EntityType.MAGMA_CUBE);
+    }
+
+    public MagmaCube(Location location, int entityId) {
+        super(location, entityId, EntityType.MAGMA_CUBE);
     }
 }
