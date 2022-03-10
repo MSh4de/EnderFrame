71926f851cd2b2d819cf97a9b0bb4400f4efffd5
RealAlphaUA <realalpha.ua@gmail.com>
2022-03-11 00:36:09 +0100

    convert to EntityMetadataBucket 🎁

--- src/main/java/eu/mshade/enderframe/entity/Ghast.java
+++ src/main/java/eu/mshade/enderframe/entity/Ghast.java
@@ -7,18 +7,21 @@ import java.util.UUID;
 
 public abstract class Ghast extends LivingEntity {
 
+
+    public Ghast(Location location, Vector velocity, int entityId, UUID uuid ) {
+        super(location, velocity, entityId, uuid, EntityType.GHAST);
+    }
+
+    public Ghast(Location location, int entityId) {
+        super(location, entityId, EntityType.GHAST);
+    }
+
+    /*
     protected boolean attacking;
 
-    public Ghast(Location beforeLocation, Location location, Vector velocity, int entityId, boolean fire, boolean sneaking, boolean sprinting, boolean eating, boolean invisible, short airTicks, String customName, boolean customNameVisible, boolean silent, boolean invulnerable, UUID uuid, EntityType entityType, float health, int potionEffectColor, boolean potionEffectAmbient, byte numberOfArrowInEntity, boolean ai, boolean attacking) {
-        super(beforeLocation, location, velocity, entityId, fire, sneaking, sprinting, eating, invisible, airTicks, customName, customNameVisible, silent, invulnerable, uuid, entityType, health, potionEffectColor, potionEffectAmbient, numberOfArrowInEntity, ai);
-        this.attacking = attacking;
-    }
+     */
 
-    public boolean isAttacking() {
-        return attacking;
-    }
 
-    public void setAttacking(boolean attacking) {
-        this.attacking = attacking;
-    }
+
 }
+
