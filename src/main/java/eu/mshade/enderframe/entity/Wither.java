b5516083a3f6ff6871b247a05f6925240aa3b332
RealAlphaUA <realalpha.ua@gmail.com>
2022-03-11 00:38:10 +0100

    convert to EntityMetadataBucket 🎁

--- src/main/java/eu/mshade/enderframe/entity/Wither.java
+++ src/main/java/eu/mshade/enderframe/entity/Wither.java
@@ -6,29 +6,18 @@ import eu.mshade.enderframe.world.Vector;
 import java.util.UUID;
 
 public abstract class Wither extends LivingEntity {
-
+    /*
     protected int watchedTarget;
     protected int invulnerableTime;
 
-    public Wither(Location beforeLocation, Location location, Vector velocity, int entityId, boolean fire, boolean sneaking, boolean sprinting, boolean eating, boolean invisible, short airTicks, String customName, boolean customNameVisible, boolean silent, boolean invulnerable, UUID uuid, EntityType entityType, float health, int potionEffectColor, boolean potionEffectAmbient, byte numberOfArrowInEntity, boolean ai, int watchedTarget, int invulnerableTime) {
-        super(beforeLocation, location, velocity, entityId, fire, sneaking, sprinting, eating, invisible, airTicks, customName, customNameVisible, silent, invulnerable, uuid, entityType, health, potionEffectColor, potionEffectAmbient, numberOfArrowInEntity, ai);
-        this.watchedTarget = watchedTarget;
-        this.invulnerableTime = invulnerableTime;
+     */
+
+    public Wither(Location location, Vector velocity, int entityId, UUID uuid) {
+        super(location, velocity, entityId, uuid, EntityType.WITHER);
     }
 
-    public int getWatchedTarget() {
-        return watchedTarget;
+    public Wither(Location location, int entityId) {
+        super(location, entityId, EntityType.WITHER);
     }
 
-    public void setWatchedTarget(int watchedTarget) {
-        this.watchedTarget = watchedTarget;
-    }
-
-    public int getInvulnerableTime() {
-        return invulnerableTime;
-    }
-
-    public void setInvulnerableTime(int invulnerableTime) {
-        this.invulnerableTime = invulnerableTime;
-    }
 }
