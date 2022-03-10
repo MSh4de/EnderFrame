dbbc3d8920d33f908bf16969056ee3f5db9d4d37
RealAlphaUA <realalpha.ua@gmail.com>
2022-03-11 00:38:12 +0100

    convert to EntityMetadataBucket 🎁

--- src/main/java/eu/mshade/enderframe/entity/Wolf.java
+++ src/main/java/eu/mshade/enderframe/entity/Wolf.java
@@ -7,40 +7,20 @@ import java.util.UUID;
 
 public abstract class Wolf extends LivingEntity implements Tameable {
 
-
+    /*
     protected boolean isAngry;
     protected boolean begging;
     protected boolean collarColor;
 
-    public Wolf(Location beforeLocation, Location location, Vector velocity, int entityId, boolean fire, boolean sneaking, boolean sprinting, boolean eating, boolean invisible, short airTicks, String customName, boolean customNameVisible, boolean silent, boolean invulnerable, UUID uuid, EntityType entityType, float health, int potionEffectColor, boolean potionEffectAmbient, byte numberOfArrowInEntity, boolean ai, boolean isAngry, boolean begging, boolean collarColor) {
-        super(beforeLocation, location, velocity, entityId, fire, sneaking, sprinting, eating, invisible, airTicks, customName, customNameVisible, silent, invulnerable, uuid, entityType, health, potionEffectColor, potionEffectAmbient, numberOfArrowInEntity, ai);
-        this.isAngry = isAngry;
-        this.begging = begging;
-        this.collarColor = collarColor;
+     */
+
+    public Wolf(Location location, Vector velocity, int entityId, UUID uuid) {
+        super(location, velocity, entityId, uuid, EntityType.WOLF);
     }
 
-    public boolean isAngry() {
-        return this.isAngry;
+    public Wolf(Location location, int entityId) {
+        super(location, entityId, EntityType.WOLF);
     }
 
-    public void setAngry(boolean angry) {
-        this.isAngry = angry;
-    }
-
-    public boolean isBegging() {
-        return begging;
-    }
-
-    public void setBegging(boolean begging) {
-        this.begging = begging;
-    }
-
-    public boolean isCollarColor() {
-        return collarColor;
-    }
-
-    public void setCollarColor(boolean collarColor) {
-        this.collarColor = collarColor;
-    }
 
 }
