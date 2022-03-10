dc02468b92a6cf7430f3ee09200612f166e5c677
RealAlphaUA <realalpha.ua@gmail.com>
2022-03-11 00:37:27 +0100

    convert to EntityMetadataBucket 🎁

--- src/main/java/eu/mshade/enderframe/entity/Mooshroom.java
+++ src/main/java/eu/mshade/enderframe/entity/Mooshroom.java
@@ -7,7 +7,12 @@ import java.util.UUID;
 
 public abstract class Mooshroom extends LivingEntity {
 
-    public Mooshroom(Location beforeLocation, Location location, Vector velocity, int entityId, boolean fire, boolean sneaking, boolean sprinting, boolean eating, boolean invisible, short airTicks, String customName, boolean customNameVisible, boolean silent, boolean invulnerable, UUID uuid, EntityType entityType, float health, int potionEffectColor, boolean potionEffectAmbient, byte numberOfArrowInEntity, boolean ai) {
-        super(beforeLocation, location, velocity, entityId, fire, sneaking, sprinting, eating, invisible, airTicks, customName, customNameVisible, silent, invulnerable, uuid, entityType, health, potionEffectColor, potionEffectAmbient, numberOfArrowInEntity, ai);
+
+    public Mooshroom(Location location, Vector velocity, int entityId, UUID uuid) {
+        super(location, velocity, entityId, uuid, EntityType.MOOSHROOM);
+    }
+
+    public Mooshroom(Location location, int entityId) {
+        super(location, entityId, EntityType.MOOSHROOM);
     }
 }
