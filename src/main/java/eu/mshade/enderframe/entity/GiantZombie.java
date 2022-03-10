5a9a0b3f21df3bc2c0a0d475a1415e7c1df92726
RealAlphaUA <realalpha.ua@gmail.com>
2022-03-11 00:36:14 +0100

    convert to EntityMetadataBucket 🎁

--- src/main/java/eu/mshade/enderframe/entity/GiantZombie.java
+++ src/main/java/eu/mshade/enderframe/entity/GiantZombie.java
@@ -7,7 +7,12 @@ import java.util.UUID;
 
 public abstract class GiantZombie extends LivingEntity {
 
-    public GiantZombie(Location beforeLocation, Location location, Vector velocity, int entityId, boolean fire, boolean sneaking, boolean sprinting, boolean eating, boolean invisible, short airTicks, String customName, boolean customNameVisible, boolean silent, boolean invulnerable, UUID uuid, EntityType entityType, float health, int potionEffectColor, boolean potionEffectAmbient, byte numberOfArrowInEntity, boolean ai) {
-        super(beforeLocation, location, velocity, entityId, fire, sneaking, sprinting, eating, invisible, airTicks, customName, customNameVisible, silent, invulnerable, uuid, entityType, health, potionEffectColor, potionEffectAmbient, numberOfArrowInEntity, ai);
+
+    public GiantZombie(Location location, Vector velocity, int entityId, UUID uuid) {
+        super(location, velocity, entityId, uuid, EntityType.GIANT);
+    }
+
+    public GiantZombie(Location location, int entityId) {
+        super(location, entityId, EntityType.GIANT);
     }
 }
