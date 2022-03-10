3dd228b70f5e4711f1a72b2ac0ad63cc5d6261a7
RealAlphaUA <realalpha.ua@gmail.com>
2022-03-11 00:37:56 +0100

    convert to EntityMetadataBucket 🎁

--- src/main/java/eu/mshade/enderframe/entity/Skeleton.java
+++ src/main/java/eu/mshade/enderframe/entity/Skeleton.java
@@ -6,20 +6,19 @@ import eu.mshade.enderframe.world.Vector;
 import java.util.UUID;
 
 public abstract class Skeleton extends LivingEntity implements ProjectileSource {
+    public Skeleton(Location location, Vector velocity, int entityId, UUID uuid) {
+        super(location, velocity, entityId, uuid, EntityType.SKELETON);
+    }
 
+    public Skeleton(Location location, int entityId) {
+        super(location, entityId, EntityType.SKELETON);
+    }
+
+    /*
     protected SkeletonType skeletonType;
 
-    public Skeleton(Location beforeLocation, Location location, Vector velocity, int entityId, boolean fire, boolean sneaking, boolean sprinting, boolean eating, boolean invisible, short airTicks, String customName, boolean customNameVisible, boolean silent, boolean invulnerable, UUID uuid, EntityType entityType, float health, int potionEffectColor, boolean potionEffectAmbient, byte numberOfArrowInEntity, boolean ai, SkeletonType skeletonType) {
-        super(beforeLocation, location, velocity, entityId, fire, sneaking, sprinting, eating, invisible, airTicks, customName, customNameVisible, silent, invulnerable, uuid, entityType, health, potionEffectColor, potionEffectAmbient, numberOfArrowInEntity, ai);
-        this.skeletonType = skeletonType;
-    }
+     */
 
-    public SkeletonType getSkeletonType() {
-        return skeletonType;
-    }
 
-    public void setSkeletonType(SkeletonType skeletonType) {
-        this.skeletonType = skeletonType;
-    }
 
 }
