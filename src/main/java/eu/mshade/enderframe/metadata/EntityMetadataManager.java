fe70bfd16b5895a13b9a786043cae659418bf211
RealAlphaUA <realalpha.ua@gmail.com>
2022-03-21 00:43:56 +0100

    change type of value 🎢

--- src/main/java/eu/mshade/enderframe/metadata/EntityMetadataManager.java
+++ src/main/java/eu/mshade/enderframe/metadata/EntityMetadataManager.java
@@ -7,15 +7,15 @@ import java.util.Map;
 
 public abstract class EntityMetadataManager {
 
-    private Map<EntityType, EntityMetadataController> entityMetadataControllerByType = new HashMap<>();
+    private Map<EntityType, EntityMetadataBucket> entityMetadataControllerByType = new HashMap<>();
     private final Map<MetadataType, Integer> indexByMetadataType = new HashMap<>();
     private final Map<MetadataType, MetadataBuffer<?>> metadataBufferByType = new HashMap<>();
 
-    public void registerEntityMetadataController(EntityType entityType, EntityMetadataController entityMetadataController){
-        this.entityMetadataControllerByType.put(entityType, entityMetadataController);
+    public void registerEntityMetadataController(EntityType entityType, EntityMetadataBucket entityMetadataBucket){
+        this.entityMetadataControllerByType.put(entityType, entityMetadataBucket);
     }
 
-    public void registerMetadataIndex( int index, MetadataType metadataType, MetadataBuffer<?> metadataBuffer) {
+    public void registerMetadataIndex(int index, MetadataType metadataType, MetadataBuffer<?> metadataBuffer) {
         this.indexByMetadataType.put(metadataType, index);
         this.metadataBufferByType.put(metadataType, metadataBuffer);
     }
