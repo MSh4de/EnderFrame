5976af4793065c97a7bfb9bffe3c6712b0509b3a
RealAlphaUA <realalpha.ua@gmail.com>
2022-03-21 00:44:13 +0100

    using generic type 🎢

--- src/main/java/eu/mshade/enderframe/metadata/EntityMetadataBuffer.java
+++ src/main/java/eu/mshade/enderframe/metadata/EntityMetadataBuffer.java
@@ -3,8 +3,8 @@ package eu.mshade.enderframe.metadata;
 import eu.mshade.enderframe.entity.Entity;
 import eu.mshade.enderframe.protocol.ByteMessage;
 
-public interface EntityMetadataBuffer {
+public interface EntityMetadataBuffer<T extends Entity> {
 
-    void write(ByteMessage byteMessage, Entity entity);
+    void write(ByteMessage byteMessage, T t);
 
 }
