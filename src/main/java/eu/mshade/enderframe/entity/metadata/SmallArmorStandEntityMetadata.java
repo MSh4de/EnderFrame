cf0a49ef62df1541ac052da392641d2e37262c3c
RealAlphaUA <realalpha.ua@gmail.com>
2022-03-28 00:16:58 +0200

    add SmallArmorStandEntityMetadata 🎁

---
+++ src/main/java/eu/mshade/enderframe/entity/metadata/SmallArmorStandEntityMetadata.java
@@ -0,0 +1,12 @@
+package eu.mshade.enderframe.entity.metadata;
+
+import eu.mshade.enderframe.metadata.EntityMetadata;
+import eu.mshade.enderframe.metadata.EntityMetadataType;
+
+public class SmallArmorStandEntityMetadata extends EntityMetadata<Boolean> {
+
+    public SmallArmorStandEntityMetadata(Boolean aBoolean) {
+        super(aBoolean, EntityMetadataType.SMALL_ARMOR_STAND);
+    }
+
+}
