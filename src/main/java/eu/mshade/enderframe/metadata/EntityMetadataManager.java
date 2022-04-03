826c4b368de0911bb0f0b8fd2af9ea0f616f5f76
RealAlphaUA <realalpha.ua@gmail.com>
2022-04-04 00:45:48 +0200

    update name of field 🦺

--- src/main/java/eu/mshade/enderframe/metadata/EntityMetadataManager.java
+++ src/main/java/eu/mshade/enderframe/metadata/EntityMetadataManager.java
@@ -1,5 +1,6 @@
 package eu.mshade.enderframe.metadata;
 
+import eu.mshade.enderframe.entity.Entity;
 import eu.mshade.enderframe.entity.EntityType;
 
 import java.util.HashMap;
@@ -7,17 +8,29 @@ import java.util.Map;
 
 public abstract class EntityMetadataManager {
 
-    private Map<EntityType, EntityMetadataBucket> entityMetadataControllerByType = new HashMap<>();
+    private Map<EntityType, EntityMetadataBucket> entityMetadataBucketByType = new HashMap<>();
     private final Map<MetadataType, Integer> indexByMetadataType = new HashMap<>();
     private final Map<MetadataType, MetadataBuffer<?>> metadataBufferByType = new HashMap<>();
 
-    public void registerEntityMetadataController(EntityType entityType, EntityMetadataBucket entityMetadataBucket){
-        this.entityMetadataControllerByType.put(entityType, entityMetadataBucket);
+    public void registerEntityMetadataBucket(EntityType entityType, EntityMetadataBucket entityMetadataBucket){
+        this.entityMetadataBucketByType.put(entityType, entityMetadataBucket);
+    }
+
+    public EntityMetadataBucket getEntityMetadataBucket(Entity entity){
+        return getEntityMetadataBucket(entity.getEntityType());
+    }
+
+    public EntityMetadataBucket getEntityMetadataBucket(EntityType entityType){
+        return this.entityMetadataBucketByType.get(entityType);
     }
 
     public void registerMetadataIndex(int index, MetadataType metadataType, MetadataBuffer<?> metadataBuffer) {
         this.indexByMetadataType.put(metadataType, index);
         this.metadataBufferByType.put(metadataType, metadataBuffer);
+    }
+
+    public MetadataBuffer<?> getMetadataBuffer(MetadataType metadataType){
+        return this.metadataBufferByType.get(metadataType);
     }
 
     public int getMetadataIndex(MetadataType metadataType){
