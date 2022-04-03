c7bdd8cd7ef19c5c4f50d5841a1e1687896fb6e3
RealAlphaUA <realalpha.ua@gmail.com>
2022-04-04 01:03:12 +0200

    remove unused import 🎐

--- src/main/java/eu/mshade/enderframe/entity/NonPlayerCharacter.java
+++ src/main/java/eu/mshade/enderframe/entity/NonPlayerCharacter.java
@@ -1,14 +1,8 @@
 package eu.mshade.enderframe.entity;
 
-import eu.mshade.enderframe.GameMode;
-import eu.mshade.enderframe.mojang.GameProfile;
-import eu.mshade.enderframe.mojang.SkinParts;
-import eu.mshade.enderframe.protocol.ProtocolVersion;
 import eu.mshade.enderframe.world.Location;
 import eu.mshade.enderframe.world.Vector;
 
-import java.net.SocketAddress;
-import java.util.Optional;
 import java.util.UUID;
 
 public abstract class NonPlayerCharacter extends Player{
