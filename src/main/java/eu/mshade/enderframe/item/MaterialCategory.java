68d326671babfa440c0883d519915dd788e98f7e
RealAlphaUA <realalpha.ua@gmail.com>
2022-07-14 23:45:27 +0200

    add materialCategory 🎞

---
+++ src/main/java/eu/mshade/enderframe/item/MaterialCategory.java
@@ -0,0 +1,9 @@
+package eu.mshade.enderframe.item;
+
+public class MaterialCategory {
+
+    public static MaterialCategoryKey BLOCK = MaterialCategoryKey.from("BLOCK");
+    public static MaterialCategoryKey ARMOR = MaterialCategoryKey.from("ARMOR");
+    public static MaterialCategoryKey TOOLS = MaterialCategoryKey.from("TOOLS");
+
+}
---
+++ src/main/java/eu/mshade/enderframe/item/MaterialCategoryKey.java
@@ -0,0 +1,46 @@
+package eu.mshade.enderframe.item;
+
+import java.util.Objects;
+
+public interface MaterialCategoryKey {
+
+    String getName();
+
+    static MaterialCategoryKey from(String name){
+        return new DefaultMaterialCategoryKey(name);
+    }
+
+     class DefaultMaterialCategoryKey implements MaterialCategoryKey {
+
+        private final String name;
+        public DefaultMaterialCategoryKey(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String getName() {
+            return this.name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null) return false;
+            if(!(o instanceof MaterialCategoryKey)) return false;
+            MaterialCategoryKey materialCategoryKey = (MaterialCategoryKey) o;
+            return Objects.equals(name, materialCategoryKey.getName());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+
+        @Override
+        public String toString() {
+            return "DefaultMaterialCategoryKey{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+    }
+
+}
