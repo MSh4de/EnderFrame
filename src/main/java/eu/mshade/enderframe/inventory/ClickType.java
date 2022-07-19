b53b766fb1e77eb358934b64b0c7141e137979d3
RealAlphaUA <realalpha.ua@gmail.com>
2022-07-19 22:43:34 +0200

    add ClickType 🎍

---
+++ src/main/java/eu/mshade/enderframe/inventory/ClickType.java
@@ -0,0 +1,19 @@
+package eu.mshade.enderframe.inventory;
+
+public enum ClickType {
+
+    CONTROL_DROP,
+    CREATIVE,
+    DOUBLE_CLICK,
+    DROP,
+    LEFT,
+    MIDDLE,
+    NUMBER_KEY,
+    RIGHT,
+    SHIFT_LEFT,
+    SHIFT_RIGHT,
+    UNKNOWN,
+    WINDOW_BORDER_LEFT,
+    WINDOW_BORDER_RIGHT;
+
+}
