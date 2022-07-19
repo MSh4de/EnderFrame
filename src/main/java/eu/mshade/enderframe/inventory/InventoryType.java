05753291ec942db1308e141b14f5813bb3a92248
RealAlphaUA <realalpha.ua@gmail.com>
2022-07-19 22:51:13 +0200

    register new InventoryKey 🛒

--- src/main/java/eu/mshade/enderframe/inventory/InventoryType.java
+++ src/main/java/eu/mshade/enderframe/inventory/InventoryType.java
@@ -3,6 +3,7 @@ package eu.mshade.enderframe.inventory;
 public class InventoryType {
 
     public static InventoryKey CHEST =  InventoryKey.from("CHEST", 9*3);
+    public static InventoryKey PLAYER = InventoryKey.from("PLAYER", 46);
 
 
 }
