48788063fea46def791c12573c5643e059ff46e5
RealAlphaUA <realalpha.ua@gmail.com>
2022-07-19 22:43:15 +0200

    add playerInventory 👓

---
+++ src/main/java/eu/mshade/enderframe/inventory/PlayerInventory.java
@@ -0,0 +1,27 @@
+package eu.mshade.enderframe.inventory;
+
+import eu.mshade.enderframe.item.ItemStack;
+import eu.mshade.enderframe.mojang.chat.TextComponent;
+
+public class PlayerInventory extends Inventory {
+
+    public PlayerInventory() {
+        super(TextComponent.of(""), InventoryType.PLAYER);
+    }
+
+    @Override
+    public void setItemStack(int slot, ItemStack itemStack) {
+        System.out.println(accurateSlot(slot));
+        super.setItemStack(accurateSlot(slot), itemStack);
+    }
+
+    public static int accurateSlot(int index) {
+        if (index <= 8) {
+            return index + 36;
+        } else if (index >= 36 && index <= 44) {
+            return 44 - index;
+        }
+        return index;
+    }
+
+}
