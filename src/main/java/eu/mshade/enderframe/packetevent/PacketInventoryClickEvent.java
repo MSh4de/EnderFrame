0e242b24dd228977d719306c73237caa00d82075
RealAlphaUA <realalpha.ua@gmail.com>
2022-07-21 15:39:06 +0200

    create object 🎡

--- src/main/java/eu/mshade/enderframe/packetevent/PacketInventoryClickEvent.java
+++ src/main/java/eu/mshade/enderframe/packetevent/PacketInventoryClickEvent.java
@@ -9,6 +9,33 @@ public class PacketInventoryClickEvent implements PacketEvent {
     protected ClickType clickType;
     protected int slot;
     protected int key;
-    
 
+    public PacketInventoryClickEvent(Inventory inventory, ClickType clickType, int slot, int key) {
+        this.inventory = inventory;
+        this.clickType = clickType;
+        this.slot = slot;
+        this.key = key;
+    }
+
+    public PacketInventoryClickEvent(Inventory inventory, ClickType clickType, int slot) {
+        this.inventory = inventory;
+        this.clickType = clickType;
+        this.slot = slot;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public ClickType getClickType() {
+        return clickType;
+    }
+
+    public int getSlot() {
+        return slot;
+    }
+
+    public int getKey() {
+        return key;
+    }
 }
