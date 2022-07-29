2107612a66cfff75709e30ad704714ba962aceef
RealAlphaUA <realalpha.ua@gmail.com>
2022-07-29 22:14:06 +0200

    adds inventoryType 🥼

--- src/main/java/eu/mshade/enderframe/inventory/InventoryType.java
+++ src/main/java/eu/mshade/enderframe/inventory/InventoryType.java
@@ -3,7 +3,15 @@ package eu.mshade.enderframe.inventory;
 public class InventoryType {
 
     public static InventoryKey CHEST =  InventoryKey.from("CHEST", 9*3);
+    public static InventoryKey HOPPER =  InventoryKey.from("HOPPER", 5);
     public static InventoryKey PLAYER = InventoryKey.from("PLAYER", 46);
+    public static InventoryKey WORKBENCH = InventoryKey.from("WORKBENCH", 3*3);
+    public static InventoryKey FURNACE = InventoryKey.from("FURNACE", 3*3);
+    public static InventoryKey DISPENSER = InventoryKey.from("DISPENSER", 3*3);
+    public static InventoryKey ENCHANTMENT_TABLE = InventoryKey.from("ENCHANTMENT_TABLE", 2);
+    public static InventoryKey BREWING_STAND = InventoryKey.from("BREWING_STAND", 4);
+    public static InventoryKey VILLAGER = InventoryKey.from("VILLAGER", 4);
+    public static InventoryKey BEACON = InventoryKey.from("BEACON", 4);
 
 
 }
