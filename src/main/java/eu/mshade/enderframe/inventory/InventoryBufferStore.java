05c5adf605e7718b916df8f6717d07272ec0a4af
RealAlphaUA <realalpha.ua@gmail.com>
2022-08-01 00:20:09 +0200

    add placedItemStack for track draging action 🎢

--- src/main/java/eu/mshade/enderframe/inventory/InventoryBufferStore.java
+++ src/main/java/eu/mshade/enderframe/inventory/InventoryBufferStore.java
@@ -2,10 +2,16 @@ package eu.mshade.enderframe.inventory;
 
 import eu.mshade.enderframe.item.ItemStack;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class InventoryBufferStore {
 
     protected ItemStack pickedItemStack;
+    protected int pickedSlot = 0;
     protected ClickType lastClickType = ClickType.UNKNOWN;
+    protected List<PlacedItemStack> placedItemStacks = new ArrayList<>();
 
     public ItemStack getPickedItemStack() {
         return pickedItemStack;
@@ -15,6 +21,14 @@ public class InventoryBufferStore {
         this.pickedItemStack = pickedItemStack;
     }
 
+    public int getPickedSlot() {
+        return pickedSlot;
+    }
+
+    public void setPickedSlot(int pickedSlot) {
+        this.pickedSlot = pickedSlot;
+    }
+
     public ClickType getLastClickType() {
         return lastClickType;
     }
@@ -22,4 +36,40 @@ public class InventoryBufferStore {
     public void setLastClickType(ClickType lastClickType) {
         this.lastClickType = lastClickType;
     }
+
+    public void addPlacedItemStack(int slot, boolean playerInventory){
+        this.placedItemStacks.add(new PlacedItemStack(slot, playerInventory));
+    }
+
+    public Collection<PlacedItemStack> getPLacedItemStacks(){
+        return this.placedItemStacks;
+    }
+
+    public void clearPLacedItemStacks(){
+        this.placedItemStacks.clear();;
+    }
+
+    public static class PlacedItemStack {
+
+        protected int slot;
+        protected boolean playerInventory;
+
+        public PlacedItemStack(int slot, boolean playerInventory) {
+            this.slot = slot;
+            this.playerInventory = playerInventory;
+        }
+
+        public PlacedItemStack(int slot) {
+            this.slot = slot;
+        }
+
+        public int getSlot() {
+            return slot;
+        }
+
+        public boolean isPlayerInventory() {
+            return playerInventory;
+        }
+    }
+
 }
