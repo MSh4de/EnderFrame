b37f4c9c8cb6dfd42cc83a239fbecd4dfdfd2406
Toky <tokydevfr@gmail.com>
2022-08-02 19:11:53 +0200

    Start world border 🎉

---
+++ src/main/java/eu/mshade/enderframe/world/border/WorldBorderAction.java
@@ -0,0 +1,21 @@
+package eu.mshade.enderframe.world.border;
+
+public enum WorldBorderAction {
+
+    SET_SIZE(0),
+    LERP_SIZE(1),
+    SET_CENTER(2),
+    INITIALIZE(3),
+    SET_WARNING_TIME(4),
+    SET_WARNING_BLOCKS(5);
+
+    private final int action;
+
+    WorldBorderAction(int action) {
+        this.action = action;
+    }
+
+    public int getAction() {
+        return action;
+    }
+}
