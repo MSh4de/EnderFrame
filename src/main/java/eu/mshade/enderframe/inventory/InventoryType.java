92227d4d635c3a79d05b27a71cdf81601732cf98
RealAlphaUA <realalpha.ua@gmail.com>
2022-08-04 23:48:23 +0200

    change size of player inventory 🥽

--- src/main/java/eu/mshade/enderframe/inventory/InventoryType.java
+++ src/main/java/eu/mshade/enderframe/inventory/InventoryType.java
@@ -4,7 +4,7 @@ public class InventoryType {
 
     public static InventoryKey CHEST =  InventoryKey.from("CHEST", 9*3);
     public static InventoryKey HOPPER =  InventoryKey.from("HOPPER", 5);
-    public static InventoryKey PLAYER = InventoryKey.from("PLAYER", 46);
+    public static InventoryKey PLAYER = InventoryKey.from("PLAYER", 45);
     public static InventoryKey WORKBENCH = InventoryKey.from("WORKBENCH", 3*3);
     public static InventoryKey FURNACE = InventoryKey.from("FURNACE", 3*3);
     public static InventoryKey DISPENSER = InventoryKey.from("DISPENSER", 3*3);
