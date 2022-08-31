a1186e00b1332f75d33337fbf2520144e0adedb3
RealAlphaUA <realalpha.ua@gmail.com>
2022-08-31 22:59:42 +0200

    convert Wrapper.java to .kt 🎠

---
+++ src/main/java/eu/mshade/enderframe/wrapper/Wrapper.kt
@@ -0,0 +1,29 @@
+package eu.mshade.enderframe.wrapper
+
+open class Wrapper<I, O> {
+
+    private var outputByInput: MutableMap<I, O> = HashMap()
+    private var inputByOutput: MutableMap<O, I> = HashMap()
+
+    fun wrap(input: I): O? {
+        return outputByInput[input]
+    }
+
+    fun reverse(output: O): I? {
+        return inputByOutput[output]
+    }
+
+    fun register(i: I, o: O) {
+        inputByOutput[o] = i
+        outputByInput[i] = o
+    }
+
+    fun registerInput(i: I, o: O) {
+        outputByInput[i] = o
+    }
+
+    fun registerOutput(o: O, i: I) {
+        inputByOutput[o] = i
+    }
+
+}
