4ede7918353d2f7bfcf351781c09b8d7c686bda9
RealAlphaUA <realalpha.ua@gmail.com>
2022-09-05 00:02:06 +0200

    change line by row 🛒

--- src/main/java/eu/mshade/enderframe/inventory/ChestInventory.java
+++ src/main/java/eu/mshade/enderframe/inventory/ChestInventory.java
@@ -1,20 +1,23 @@
 package eu.mshade.enderframe.inventory;
 
+import eu.mshade.enderframe.item.ItemStack;
 import eu.mshade.enderframe.mojang.chat.TextComponent;
 
 public class ChestInventory extends Inventory {
 
-    protected int line;
+    protected final int row;
 
-    public ChestInventory(TextComponent textComponent) {
-        super(textComponent, InventoryType.CHEST);
+    public ChestInventory(TextComponent textComponent, int row) {
+        super(textComponent, InventoryType.CHEST, new ItemStack[row * 9]);
+        this.row = row;
     }
 
-    public ChestInventory(String name) {
-        super(name, InventoryType.CHEST);
+    public ChestInventory(String name, int row) {
+        super(name, InventoryType.CHEST, new ItemStack[row * 9]);
+        this.row = row;
     }
 
-    public int getLine() {
-        return line;
+    public int getRow() {
+        return row;
     }
 }
