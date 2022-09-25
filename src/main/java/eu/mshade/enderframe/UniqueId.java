9187ea0e3a7cfce15ce8022e031c1383843987c9
RealAlphaUA <realalpha.ua@gmail.com>
2022-09-25 23:59:52 +0200

    add UniqueId 🥼

---
+++ src/main/java/eu/mshade/enderframe/UniqueId.java
@@ -0,0 +1,42 @@
+package eu.mshade.enderframe;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class UniqueId {
+
+    private final Queue<Integer> freeId = new ConcurrentLinkedQueue<>();
+    private final AtomicInteger atomicId;
+
+    public UniqueId() {
+        this.atomicId = new AtomicInteger(0);
+    }
+
+    public UniqueId(int offset) {
+        this.atomicId = new AtomicInteger(offset);
+    }
+
+    public UniqueId(int offset, Collection<Integer> freeId) {
+        this.atomicId = new AtomicInteger(offset);
+        this.freeId.addAll(freeId);
+    }
+
+    public Integer getFreeId() {
+        return Optional.ofNullable(freeId.poll()).orElseGet(atomicId::getAndIncrement);
+    }
+
+    public void flushId(int id) {
+        freeId.add(id);
+    }
+
+    public AtomicInteger getAtomicId() {
+        return atomicId;
+    }
+
+    public Queue<Integer> getFreeIdQueue() {
+        return freeId;
+    }
+}
