79dfca6bbf60b74ce782fa8a778d6341b959b958
RealAlphaUA <realalpha.ua@gmail.com>
2022-09-26 00:01:09 +0200

    add prefix BLOCK at type of metadata block and add ExtraBlockMetadata 🎠

--- src/main/java/eu/mshade/enderframe/world/block/BlockMetadata.kt
+++ src/main/java/eu/mshade/enderframe/world/block/BlockMetadata.kt
@@ -2,17 +2,21 @@ package eu.mshade.enderframe.world.block
 
 import eu.mshade.enderframe.metadata.MetadataKey
 import eu.mshade.enderframe.metadata.MetadataKeyValue
+import eu.mshade.mwork.binarytag.entity.CompoundBinaryTag
 
 object BlockMetadataType {
 
-    val LIGHT: MetadataKey = MetadataKey.from("LIGHT")
-    val FACE: MetadataKey = MetadataKey.from("FACE")
+    val LIGHT: MetadataKey = MetadataKey.from("BLOCK_LIGHT")
+    val FACE: MetadataKey = MetadataKey.from("BLOCK_FACE")
+    val EXTRA: MetadataKey = MetadataKey.from("BLOCK_EXTRA")
 
 }
 
 class LightBlockMetadata(light: Boolean): MetadataKeyValue<Boolean>(BlockMetadataType.LIGHT, light)
 
 class FaceBlockMetadata(blockFace: BlockFace): MetadataKeyValue<BlockFace>(BlockMetadataType.FACE, blockFace)
+
+class ExtraBlockMetadata(extra: CompoundBinaryTag): MetadataKeyValue<CompoundBinaryTag>(BlockMetadataType.EXTRA, extra)
 
 
 
