bd0b13bd6e3fce078834b2098850ee497c6b8f0f
RealAlphaUA <realalpha.ua@gmail.com>
2022-10-18 22:08:22 +0200

    add class BlockTransformerRepository 🧸

--- src/main/java/eu/mshade/enderframe/world/block/BlockTransformer.kt
+++ src/main/java/eu/mshade/enderframe/world/block/BlockTransformer.kt
@@ -1,12 +1,50 @@
 package eu.mshade.enderframe.world.block
 
+import eu.mshade.enderframe.item.MaterialCategoryKey
 import eu.mshade.enderframe.item.MaterialKey
+import eu.mshade.enderframe.wrapper.Wrapper
 
 abstract class BlockTransformer {
 
-    abstract fun transform(block: Block): MaterialKey
+    abstract fun transform(block: Block, materialWrapper: Wrapper<MaterialKey, MaterialKey>): MaterialKey
 
-    abstract fun transform(materialKey: MaterialKey): Block
+    abstract fun transform(materialKey: MaterialKey, materialWrapper: Wrapper<MaterialKey, MaterialKey>): Block?
 
 }
+
+class BlockTransformerRepository {
+
+    private val transformerByMaterialKey = mutableMapOf<MaterialCategoryKey, BlockTransformer>()
+    private lateinit var defaultTransformer: BlockTransformer;
+    private lateinit var materialWrapper: Wrapper<MaterialKey, MaterialKey>;
+
+    fun registerDefaultTransformer(transformer: BlockTransformer) {
+        defaultTransformer = transformer
+    }
+
+    fun registerMaterialWrapper(materialWrapper: Wrapper<MaterialKey, MaterialKey>) {
+        this.materialWrapper = materialWrapper
+    }
+
+    fun register(materialCategoryKey: MaterialCategoryKey, blockTransformer: BlockTransformer) {
+        transformerByMaterialKey[materialCategoryKey] = blockTransformer
+    }
+
+    //server side
+    fun transform(block: Block): MaterialKey {
+        val materialKey = block.getMaterialKey()
+        val blockTransformer = transformerByMaterialKey.getOrDefault(materialKey.materialCategoryKey, defaultTransformer)
+        return blockTransformer.transform(block, materialWrapper)
+    }
+
+    //client side
+    fun reverse(materialKey: MaterialKey): Block? {
+        val materialCategoryKey = materialWrapper.reverse(MaterialKey.from(materialKey.id))?.materialCategoryKey
+        val blockTransformer = transformerByMaterialKey.getOrDefault(materialCategoryKey, defaultTransformer)
+        return blockTransformer.transform(materialKey, materialWrapper)
+    }
+
+
+}
+
 
