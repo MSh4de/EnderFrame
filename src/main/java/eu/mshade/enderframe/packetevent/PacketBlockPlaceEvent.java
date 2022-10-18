bc8e99b5c73dd610979229fe91ebfcd6dc2f4f67
RealAlphaUA <realalpha.ua@gmail.com>
2022-10-18 22:12:58 +0200

    complet PacketBlockPlaceEvent 🎑

--- src/main/java/eu/mshade/enderframe/packetevent/PacketBlockPlaceEvent.java
+++ src/main/java/eu/mshade/enderframe/packetevent/PacketBlockPlaceEvent.java
@@ -7,11 +7,13 @@ import eu.mshade.enderframe.world.block.BlockFace;
 public class PacketBlockPlaceEvent implements PacketEvent {
 
     private Vector blockPosition;
+    private Vector cursorPosition;
     private BlockFace blockFace;
     private ItemStack itemStack;
 
-    public PacketBlockPlaceEvent(Vector blockPosition, BlockFace blockFace, ItemStack itemStack) {
+    public PacketBlockPlaceEvent(Vector blockPosition, Vector cursorPosition, BlockFace blockFace, ItemStack itemStack) {
         this.blockPosition = blockPosition;
+        this.cursorPosition = cursorPosition;
         this.blockFace = blockFace;
         this.itemStack = itemStack;
     }
@@ -20,11 +22,25 @@ public class PacketBlockPlaceEvent implements PacketEvent {
         return blockPosition;
     }
 
+    public Vector getCursorPosition() {
+        return cursorPosition;
+    }
+
     public BlockFace getBlockFace() {
         return blockFace;
     }
 
     public ItemStack getItemStack() {
         return itemStack;
+    }
+
+    @Override
+    public String toString() {
+        return "PacketBlockPlaceEvent{" +
+                "blockPosition=" + blockPosition +
+                ", cursorPosition=" + cursorPosition +
+                ", blockFace=" + blockFace +
+                ", itemStack=" + itemStack +
+                '}';
     }
 }
