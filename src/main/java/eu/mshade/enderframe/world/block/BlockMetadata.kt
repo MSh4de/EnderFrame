d914a8560fcf1a9d821c75fdc58f4fa6e6a0d2c5
RealAlphaUA <realalpha.ua@gmail.com>
2022-10-18 22:09:10 +0200

    add blockMetadata like powered, axis, half, shape(soon delete?) 🎏

--- src/main/java/eu/mshade/enderframe/world/block/BlockMetadata.kt
+++ src/main/java/eu/mshade/enderframe/world/block/BlockMetadata.kt
@@ -6,17 +6,27 @@ import eu.mshade.mwork.binarytag.entity.CompoundBinaryTag
 
 object BlockMetadataType {
 
-    val LIGHT: MetadataKey = MetadataKey.from("BLOCK_LIGHT")
     val FACE: MetadataKey = MetadataKey.from("BLOCK_FACE")
     val EXTRA: MetadataKey = MetadataKey.from("BLOCK_EXTRA")
+    val HALF : MetadataKey = MetadataKey.from("BLOCK_HALF")
+    val SHAPE: MetadataKey = MetadataKey.from("BLOCK_SHAPE")
+    val AXIS: MetadataKey = MetadataKey.from("BLOCK_AXIS")
+    val POWER: MetadataKey = MetadataKey.from("BLOCK_POWER")
+    val POWERED: MetadataKey = MetadataKey.from("BLOCK_POWERED")
 
 }
 
-class LightBlockMetadata(light: Boolean): MetadataKeyValue<Boolean>(BlockMetadataType.LIGHT, light)
-
 class FaceBlockMetadata(blockFace: BlockFace): MetadataKeyValue<BlockFace>(BlockMetadataType.FACE, blockFace)
 
+class HalfBlockMetadata(blockHalf: BlockHalf): MetadataKeyValue<BlockHalf>(BlockMetadataType.HALF, blockHalf)
+
+class ShapeBlockMetadata(blockShape: BlockShape): MetadataKeyValue<BlockShape>(BlockMetadataType.SHAPE, blockShape)
+
 class ExtraBlockMetadata(extra: CompoundBinaryTag): MetadataKeyValue<CompoundBinaryTag>(BlockMetadataType.EXTRA, extra)
+
+class AxisBlockMetadata(blockAxis: BlockAxis): MetadataKeyValue<BlockAxis>(BlockMetadataType.AXIS, blockAxis)
+
+class PoweredBlockMetadata(powered: Boolean): MetadataKeyValue<Boolean>(BlockMetadataType.POWERED, powered)
 
 
 
