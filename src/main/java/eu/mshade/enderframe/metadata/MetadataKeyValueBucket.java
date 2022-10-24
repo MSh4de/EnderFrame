2393cb05b8d0fecb9110f6f09ae02ef0c2353ae2
RealAlphaUA <realalpha.ua@gmail.com>
2022-10-24 23:53:12 +0200

    fix null 🧵

--- src/main/java/eu/mshade/enderframe/metadata/MetadataKeyValueBucket.java
+++ src/main/java/eu/mshade/enderframe/metadata/MetadataKeyValueBucket.java
@@ -1,14 +1,26 @@
 package eu.mshade.enderframe.metadata;
 
+import eu.mshade.mwork.PrettyString;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.*;
 
-public class MetadataKeyValueBucket {
+public class MetadataKeyValueBucket implements PrettyString {
 
     protected final Map<MetadataKey, MetadataKeyValue<?>> metadataKeyValueByMetadataKey = new HashMap<>();
     protected final Set<MetadataKey> metadataKeyUpdated = new HashSet<>();
+    protected boolean trackUpdates;
+
+    public MetadataKeyValueBucket(boolean trackUpdates) {
+        this.trackUpdates = trackUpdates;
+    }
+
+    public MetadataKeyValueBucket() {
+        this(true);
+    }
 
     public void setMetadataKeyValue(MetadataKeyValue<?> entityMetadata){
-        this.metadataKeyUpdated.add(entityMetadata.getMetadataKey());
+        if (trackUpdates) this.metadataKeyUpdated.add(entityMetadata.getMetadataKey());
         this.metadataKeyValueByMetadataKey.put(entityMetadata.getMetadataKey(), entityMetadata);
     }
 
@@ -21,11 +33,15 @@ public class MetadataKeyValueBucket {
     }
 
     public <T extends MetadataKeyValue<?>> T getMetadataKeyValue(MetadataKey metadataKey, Class<T> type){
-        return type.cast(this.metadataKeyValueByMetadataKey.get(metadataKey));
+        MetadataKeyValue<?> metadataKeyValue = this.metadataKeyValueByMetadataKey.get(metadataKey);
+        if (metadataKeyValue == null) return null;
+        return type.cast(metadataKeyValue);
     }
 
     public <T> T getValueOfMetadataKeyValue(MetadataKey metadataKey, Class<T> type){
-        return type.cast(this.metadataKeyValueByMetadataKey.get(metadataKey).getMetadataValue());
+        MetadataKeyValue<?> metadataKeyValue = this.metadataKeyValueByMetadataKey.get(metadataKey);
+        if (metadataKeyValue == null) return null;
+        return type.cast(metadataKeyValue.getMetadataValue());
     }
 
 
@@ -39,10 +55,60 @@ public class MetadataKeyValueBucket {
         return copy;
     }
 
+    public void clear(){
+        this.metadataKeyValueByMetadataKey.clear();
+        this.metadataKeyUpdated.clear();
+    }
+
+    public boolean isEmpty(){
+        return this.metadataKeyValueByMetadataKey.isEmpty();
+    }
+
+    public void toggleTrackUpdates(boolean trackUpdates){
+        this.trackUpdates = trackUpdates;
+    }
+
     @Override
     public String toString() {
         return "MetadataKeyValueBucket{" +
                 "metadataKeyValueByMetadataKey=" + metadataKeyValueByMetadataKey +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MetadataKeyValueBucket that = (MetadataKeyValueBucket) o;
+        return trackUpdates == that.trackUpdates && Objects.equals(metadataKeyValueByMetadataKey, that.metadataKeyValueByMetadataKey) && Objects.equals(metadataKeyUpdated, that.metadataKeyUpdated);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(metadataKeyValueByMetadataKey, metadataKeyUpdated, trackUpdates);
+    }
+
+
+    @NotNull
+    @Override
+    public String toPrettyString(int deep) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("MetadataKeyValueBucket{").append(System.lineSeparator());
+        stringBuilder.append(" ".repeat(deep));
+        for (MetadataKeyValue<?> metadataKeyValue : metadataKeyValueByMetadataKey.values()) {
+            stringBuilder.append(" ".repeat(deep+1));
+            stringBuilder.append(metadataKeyValue.getMetadataKey().getName()).append(": ");
+            if(metadataKeyValue.getMetadataValue() instanceof PrettyString){
+                stringBuilder.append(((PrettyString) metadataKeyValue.getMetadataValue()).toPrettyString(deep + 1));
+            } else {
+                stringBuilder.append(metadataKeyValue.getMetadataValue());
+            }
+            stringBuilder.append(System.lineSeparator());
+        }
+        stringBuilder.append(" ".repeat(deep));
+        stringBuilder.append("}");
+        return stringBuilder.toString();
+    }
+
+
 }
