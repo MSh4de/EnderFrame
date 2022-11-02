4bc2f2ef571d59234e3815abbe307d67e57d1f1b
RealAlphaUA <realalpha.ua@gmail.com>
2022-11-02 23:49:26 +0100

    use java 17 🎠

--- src/main/java/eu/mshade/enderframe/protocol/PacketChannelInboundHandlerAdapter.java
+++ src/main/java/eu/mshade/enderframe/protocol/PacketChannelInboundHandlerAdapter.java
@@ -2,7 +2,6 @@ package eu.mshade.enderframe.protocol;
 
 import eu.mshade.enderframe.EnderFrame;
 import eu.mshade.enderframe.event.PlayerQuitEvent;
-import eu.mshade.mwork.ParameterContainer;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
@@ -17,9 +16,9 @@ public class PacketChannelInboundHandlerAdapter extends ChannelInboundHandlerAda
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        if (msg instanceof PacketIn packetIn){
+        if (msg instanceof MinecraftPacketIn minecraftPacketIn){
             ProtocolPipeline protocolPipeline = ProtocolPipeline.get();
-            protocolPipeline.getProtocol(channel).getEventBus().publish(packetIn);
+            protocolPipeline.getProtocol(channel).getEventBus().publish(minecraftPacketIn);
         }
     }
 
