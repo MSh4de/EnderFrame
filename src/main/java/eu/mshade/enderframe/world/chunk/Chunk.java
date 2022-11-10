f279ec7855de4c2bd26948fed330ade3b20fee44
RealAlphaUA <realalpha.ua@gmail.com>
2022-11-11 00:04:25 +0100

    add body of some methods 🖼

--- src/main/java/eu/mshade/enderframe/world/chunk/Chunk.java
+++ src/main/java/eu/mshade/enderframe/world/chunk/Chunk.java
@@ -3,21 +3,16 @@ package eu.mshade.enderframe.world.chunk;
 
 import eu.mshade.enderframe.Watchable;
 import eu.mshade.enderframe.entity.Entity;
-import eu.mshade.enderframe.entity.Player;
-import eu.mshade.enderframe.item.MaterialKey;
-import eu.mshade.enderframe.world.ChunkStatus;
+import eu.mshade.enderframe.item.Material;
 import eu.mshade.enderframe.world.World;
 import eu.mshade.enderframe.world.block.Block;
 
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.function.Consumer;
 
 public abstract class Chunk implements Watchable {
 
+    public static final int WIDTH = 16, HEIGHT = 16, DEPTH = 256, SEC_DEPTH = 16;
 
     protected final int x;
     protected final int z;
@@ -54,11 +49,27 @@ public abstract class Chunk implements Watchable {
         return this.sections;
     }
 
+    public boolean isSectionsEmpty() {
+        for (Section section : sections) {
+            if (section != null) return false;
+        }
+        return true;
+    }
+
     public ChunkStateStore getChunkStateStore() {
         return chunkStateStore;
     }
 
-    public abstract int getBitMask();
+    public int getBitMask(){
+        int sectionBitmask = 0;
+        for (int i = 0; i < sections.length; i++) {
+            Section section = sections[i];
+            if (section != null && section.getRealBlock() != 0) {
+                sectionBitmask |= 1 << i;
+            }
+        }
+        return sectionBitmask;
+    }
 
     public abstract Block getBlock(int x, int y, int z);
 
@@ -76,14 +87,41 @@ public abstract class Chunk implements Watchable {
 
     public abstract int getBiome(int x, int z);
 
-    public abstract Section getSection(int y);
+    public abstract Section getSectionOrCreate(int y);
+
+    public Section getSection(int y){
+        return sections[y];
+    }
+
+    public boolean hasSection(int y){
+        return getSection(y) != null;
+    }
+
+    public abstract Section createSection(int y);
 
     public abstract byte[] getBiomes();
 
-    public abstract int getHighest(int x, int z);
+    public int getHighest(int x, int z){
+        for (int i = 255; i > 0; i--) {
+            Block block = getBlock(x, i, z);
+            if (block == null) continue;
+            if (sections[i >> 4] != null && block.getMaterialKey() != Material.AIR) {
+                return i;
+            }
+        }
+        return 0;
+    }
 
     public static long key(int x, int z) {
         return ((long) x & 0xffffffffL) << 32 | (long) z & 0xffffffffL;
+    }
+
+    public int getBiomeIndex(int x, int z) {
+        return (z & 0xF) * WIDTH + (x & 0xF);
+    }
+
+    public int getBlockIndex(int x, int y, int z) {
+        return ((y & 0xf) << 8) | ((z & 0x0f) << 4) | (x & 0x0f);
     }
 
 }
