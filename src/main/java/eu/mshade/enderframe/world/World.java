875de46390d8a7cdcce7a21421d152fa7ecf04c2
RealAlphaUA <realalpha.ua@gmail.com>
2022-11-11 12:57:36 +0100

    add javaDoc 🎠

--- src/main/java/eu/mshade/enderframe/world/World.java
+++ src/main/java/eu/mshade/enderframe/world/World.java
@@ -4,12 +4,11 @@ import eu.mshade.enderframe.entity.Entity;
 import eu.mshade.enderframe.entity.EntityType;
 import eu.mshade.enderframe.item.MaterialKey;
 import eu.mshade.enderframe.metadata.MetadataKeyValueBucket;
-import eu.mshade.enderframe.metadata.world.WorldMetadataType;
 import eu.mshade.enderframe.tick.Tickable;
 import eu.mshade.enderframe.world.block.Block;
 import eu.mshade.enderframe.world.chunk.Chunk;
 import eu.mshade.enderframe.world.chunk.ChunkGenerator;
-import eu.mshade.mwork.binarytag.carbon.CarbonBinaryTag;
+import eu.mshade.mwork.binarytag.segment.SegmentBinaryTag;
 
 import java.io.File;
 import java.util.*;
@@ -18,6 +17,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public abstract class World extends Tickable {
+
 
     protected final File worldFolder;
     protected final File regionFolder;
@@ -40,27 +40,71 @@ public abstract class World extends Tickable {
         this(worldFolder, new MetadataKeyValueBucket());
     }
 
+    /**
+     * Flushes the chunk from memory to disk with saving the chunk data
+     * Checks method {@link #flushChunk(Chunk, boolean)} for disabling the saving
+     * @param chunk the chunk to flush
+     */
     public void flushChunk(Chunk chunk) {
         flushChunk(chunk, true);
     }
 
+    /**
+     * Saves the chunk data to disk
+     * @param chunk the chunk to save
+     */
     public abstract void saveChunk(Chunk chunk);
 
+    /**
+     * Flushes the chunk from the memory and saves it to the disk
+     * @param chunk the chunk to flush
+     * @param save whether to save the chunk or not
+     */
     public abstract void flushChunk(Chunk chunk, boolean save);
 
+    /**
+     * Gets the chunk at the given coordinates.
+     * If the chunk is not loaded, it will be loaded.
+     * If the chunk is not generated, it will be generated.
+     * @param chunkX The chunk x coordinate
+     * @param chunkZ The chunk z coordinate
+     * @return {@link CompletableFuture} of the chunk
+     */
     public abstract CompletableFuture<Chunk> getChunk(int chunkX, int chunkZ);
 
+    /**
+     * Gets chunk by id (chunkX + chunkZ * 2^32)
+     * @param id chunk id
+     * @return chunk future or null if chunk is not loaded
+     */
     public abstract CompletableFuture<Chunk> getChunk(long id);
 
-    public abstract boolean hasChunkBuffer(int x, int z);
+    /**
+     * Checks if the chunk is loaded in the world
+     * @param chunkX the chunk x
+     * @param chunkZ the chunk z
+     * @return true if the chunk is loaded
+     */
+    public abstract boolean hasChunkLoaded(int chunkX, int chunkZ);
+
+    /**
+     * Checks if the {@link Chunk} exists in the {@link World} so we're going to read it from the disk
+     * @param chunkX the chunk x
+     * @param chunkZ the chunk z
+     * @return true if the {@link Chunk} exists
+     */
+    public abstract boolean isChunkExists(int chunkX, int chunkZ);
 
     public abstract void addEntity(Entity entity);
 
     public abstract void removeEntity(Entity entity);
 
     public abstract Entity spawnEntity(EntityType entityType, Location location);
-
-    public abstract Collection<CarbonBinaryTag> getRegionBinaryTagPoets();
+    /**
+     * Gets all the regions in the world
+     * @return the regions
+     */
+    public abstract Collection<SegmentBinaryTag> getRegions();
 
     public abstract void setBlock(int x, int y, int z, MaterialKey materialKey);
 
