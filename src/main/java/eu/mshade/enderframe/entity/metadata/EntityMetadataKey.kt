e666313158f167ceb09b07959b9e2468d46e8494
Emilio B <emilio.bizouarn@gmail.com>
2022-11-14 20:42:38 +0100

    Register every metadata-key one object

---
+++ src/main/java/eu/mshade/enderframe/entity/metadata/EntityMetadataKey.kt
@@ -0,0 +1,59 @@
+package eu.mshade.enderframe.entity.metadata
+
+import eu.mshade.enderframe.metadata.MetadataKey
+
+object EntityMetadataKey {
+
+    //Register all entity metadata from 1.19
+    val ON_FIRE: MetadataKey = MetadataKey.from("ENTITY_ON_FIRE")
+    val SNEAKING: MetadataKey = MetadataKey.from("ENTITY_SNEAKING")
+    val SPRINTING: MetadataKey = MetadataKey.from("ENTITY_SPRINTING")
+    val SWIMMING: MetadataKey = MetadataKey.from("ENTITY_SWIMMING")
+    val INVISIBLE: MetadataKey = MetadataKey.from("ENTITY_INVISIBLE")
+    val GLOWING: MetadataKey = MetadataKey.from("ENTITY_GLOWING")
+    val FLYING: MetadataKey = MetadataKey.from("ENTITY_FLYING")
+    val AGE: MetadataKey = MetadataKey.from("ENTITY_AGE")
+    val AGE_LOCKED: MetadataKey = MetadataKey.from("ENTITY_AGE_LOCKED")
+    val BREEDABLE: MetadataKey = MetadataKey.from("ENTITY_BREEDABLE")
+    val SITTING: MetadataKey = MetadataKey.from("ENTITY_SITTING")
+    val TAMED: MetadataKey = MetadataKey.from("ENTITY_TAMED")
+    val OWNER: MetadataKey = MetadataKey.from("ENTITY_OWNER")
+    val SHEARED: MetadataKey = MetadataKey.from("ENTITY_SHEARED")
+    val SADDLE: MetadataKey = MetadataKey.from("ENTITY_SADDLE")
+    val CROUCHED: MetadataKey = MetadataKey.from("ENTITY_CROUCHED")
+    val HANDLING: MetadataKey = MetadataKey.from("ENTITY_HANDLING")
+    val AIR_TICKS: MetadataKey = MetadataKey.from("ENTITY_AIR_TICKS")
+    val CUSTOM_NAME: MetadataKey = MetadataKey.from("ENTITY_CUSTOM_NAME")
+    val CUSTOM_NAME_VISIBLE: MetadataKey = MetadataKey.from("ENTITY_CUSTOM_NAME_VISIBLE")
+    val SILENT: MetadataKey = MetadataKey.from("ENTITY_SILENT")
+    val CHILD: MetadataKey = MetadataKey.from("ENTITY_CHILD")
+    val VILLAGER: MetadataKey = MetadataKey.from("ENTITY_VILLAGER")
+    val CONVERTING: MetadataKey = MetadataKey.from("ENTITY_CONVERTING")
+    val HEALTH: MetadataKey = MetadataKey.from("ENTITY_HEALTH")
+    val POTION_EFFECT_COLOR: MetadataKey = MetadataKey.from("ENTITY_POTION_EFFECT_COLOR")
+    val POTION_EFFECT_AMBIENT: MetadataKey = MetadataKey.from("ENTITY_POTION_EFFECT_AMBIENT")
+    val NUMBER_OF_ARROWS_IN_ENTITY: MetadataKey = MetadataKey.from("ENTITY_NUMBER_OF_ARROWS_IN_ENTITY")
+    val WHETHER_ARTIFICIAL_INTELLIGENCE: MetadataKey = MetadataKey.from("ENTITY_WHETHER_ARTIFICIAL_INTELLIGENCE")
+    val CLIMBING: MetadataKey = MetadataKey.from("ENTITY_CLIMBING")
+    val SKIN_PART: MetadataKey = MetadataKey.from("ENTITY_SKIN_PART")
+    val ADDITIONAL_HEARTS: MetadataKey = MetadataKey.from("ENTITY_ADDITIONAL_HEARTS")
+    val SCORE: MetadataKey = MetadataKey.from("ENTITY_SCORE")
+
+    val ITEM: MetadataKey = MetadataKey.from("ENTITY_ITEM")
+
+    val TIME_SINCE_HIT: MetadataKey = MetadataKey.from("ENTITY_TIME_SINCE_HIT")
+    val FORWARD_DIRECTION: MetadataKey = MetadataKey.from("ENTITY_FORWARD_DIRECTION")
+    val DAMAGE_TAKEN: MetadataKey = MetadataKey.from("ENTITY_DAMAGE_TAKEN")
+
+    val SMALL_ARMOR_STAND: MetadataKey = MetadataKey.from("ENTITY_SMALL_ARMOR_STAND")
+    val GRAVITY: MetadataKey = MetadataKey.from("ENTITY_GRAVITY")
+    val ARMS: MetadataKey = MetadataKey.from("ENTITY_ARMS")
+    val BASE_PLATE: MetadataKey = MetadataKey.from("ENTITY_BASE_PLATE")
+    val MARKER: MetadataKey = MetadataKey.from("ENTITY_MARKER")
+    val HEAD_ROTATION: MetadataKey = MetadataKey.from("ENTITY_HEAD_ROTATION")
+    val BODY_ROTATION: MetadataKey = MetadataKey.from("ENTITY_BODY_ROTATION")
+    val LEFT_ARM_ROTATION: MetadataKey = MetadataKey.from("ENTITY_LEFT_ARM_ROTATION")
+    val RIGHT_ARM_ROTATION: MetadataKey = MetadataKey.from("ENTITY_RIGHT_ARM_ROTATION")
+    val LEFT_LEG_ROTATION: MetadataKey = MetadataKey.from("ENTITY_LEFT_LEG_ROTATION")
+    val RIGHT_LEG_ROTATION: MetadataKey = MetadataKey.from("ENTITY_RIGHT_LEG_ROTATION")
+}
