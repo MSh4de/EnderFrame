45186992abc919c5329b493d9c2f40b95325647e
Azodox <likemoi99@gmail.com>
2022-12-18 15:04:41 +0100

    Created a kotlin file that contains classes about firework metadata

---
+++ src/main/java/eu/mshade/enderframe/item/metadata/FireworkItemStackMetadata.kt
@@ -0,0 +1,12 @@
+package eu.mshade.enderframe.item.metadata
+
+import eu.mshade.enderframe.item.Firework
+import eu.mshade.enderframe.metadata.MetadataKey
+import eu.mshade.enderframe.metadata.MetadataKeyValue
+import eu.mshade.enderframe.metadata.itemstack.ItemStackMetadataKey
+
+class FireworkExplosionItemStackMetadata(metadataValue: Boolean) :
+    MetadataKeyValue<Boolean>(ItemStackMetadataKey.EXPLOSION, metadataValue)
+
+class FireworkFireworksItemStackMetadata(metadataValue: Firework) :
+    MetadataKeyValue<Firework>(ItemStackMetadataKey.FIREWORKS, metadataValue)
