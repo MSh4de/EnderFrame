0280ca8881756ec016ff1813edb9beeb601b2375
Azodox <likemoi99@gmail.com>
2022-12-18 15:03:38 +0100

    Converted java file to kotlin file and create a class for the repair cost metadata tag

--- src/main/java/eu/mshade/enderframe/item/metadata/RepairCostItemStackMetadata.kt
+++ src/main/java/eu/mshade/enderframe/item/metadata/RepairCostItemStackMetadata.kt
@@ -1,4 +1,8 @@
-package eu.mshade.enderframe.item.metadata;
+package eu.mshade.enderframe.item.metadata
 
-public class RepairCostItemStackMetadata {
-}
+import eu.mshade.enderframe.metadata.MetadataKey
+import eu.mshade.enderframe.metadata.MetadataKeyValue
+import eu.mshade.enderframe.metadata.itemstack.ItemStackMetadataKey
+
+class RepairCostItemStackMetadata(metadataValue: Int) :
+    MetadataKeyValue<Int>(ItemStackMetadataKey.REPAIR_COST, metadataValue)
