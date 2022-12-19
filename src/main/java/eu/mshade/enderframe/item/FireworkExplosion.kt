2441f600bb5afac3635614cf01c101597c6e918b
Azodox <likemoi99@gmail.com>
2022-12-19 01:12:38 +0100

    Changed IntArray parameters by Color parameters and added default value to booleans :adhesive_bandage:

--- src/main/java/eu/mshade/enderframe/item/FireworkExplosion.kt
+++ src/main/java/eu/mshade/enderframe/item/FireworkExplosion.kt
@@ -1,31 +1,5 @@
 package eu.mshade.enderframe.item
 
-data class FireworkExplosion(val flicker: Boolean?, val trail: Boolean?, val type: FireworkType, val colors: IntArray, val fadeColors: IntArray?) {
+import eu.mshade.enderframe.mojang.Color
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as FireworkExplosion
-
-        if (flicker != other.flicker) return false
-        if (trail != other.trail) return false
-        if (type != other.type) return false
-        if (!colors.contentEquals(other.colors)) return false
-        if (fadeColors != null) {
-            if (other.fadeColors == null) return false
-            if (!fadeColors.contentEquals(other.fadeColors)) return false
-        } else if (other.fadeColors != null) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = flicker?.hashCode() ?: 0
-        result = 31 * result + (trail?.hashCode() ?: 0)
-        result = 31 * result + type.hashCode()
-        result = 31 * result + colors.contentHashCode()
-        result = 31 * result + (fadeColors?.contentHashCode() ?: 0)
-        return result
-    }
-}
+data class FireworkExplosion(val flicker: Boolean = false, val trail: Boolean = false, val type: FireworkType, val color: Color, val fadeColor: Color?)
