b50d55d618d089e85d3479626308fb1d710da461
Azodox <likemoi99@gmail.com>
2022-12-20 21:40:54 +0100

    Added a method to get a firework type by its id :sparkles:

--- src/main/java/eu/mshade/enderframe/item/FireworkType.kt
+++ src/main/java/eu/mshade/enderframe/item/FireworkType.kt
@@ -5,5 +5,18 @@ enum class FireworkType(val id: Int) {
     LARGE_BALL(1),
     STAR(2),
     CREEPER(3),
-    BURST(4)
+    BURST(4);
+
+    companion object {
+        fun getFireworkType(id: Int): FireworkType {
+            return when (id) {
+                0 -> SMALL_BALL
+                1 -> LARGE_BALL
+                2 -> STAR
+                3 -> CREEPER
+                4 -> BURST
+                else -> throw IllegalArgumentException("FireworkType with id $id does not exist")
+            }
+        }
+    }
 }
