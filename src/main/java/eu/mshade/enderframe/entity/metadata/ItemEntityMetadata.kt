07ded9b1d58f5a51153eef17f7e49de03f46587c
DESKTOP-6AR26UD\emili <emilio.bizouarn@gmail.com>
2022-12-22 14:05:08 +0100

    refactor: moving the metadata class into the appropriate files then delete it

--- src/main/java/eu/mshade/enderframe/entity/metadata/ItemEntityMetadata.kt
+++
@@ -1,6 +0,0 @@
-package eu.mshade.enderframe.entity.metadata
-
-import eu.mshade.enderframe.item.ItemStack
-import eu.mshade.enderframe.metadata.MetadataKeyValue
-
-class ItemEntityMetadata(itemStack: ItemStack): MetadataKeyValue<ItemStack>(EntityMetadataKey.ITEM, itemStack)
