b5cb60e6b34deeb30d241a72610c2086a1c4fc41
RealAlphaUA <realalpha.ua@gmail.com>
2023-02-14 02:26:16 +0100

    complet ScoreboadSidebar 🎏

--- src/main/java/eu/mshade/enderframe/scoreboard/ScoreboardSidebar.kt
+++ src/main/java/eu/mshade/enderframe/scoreboard/ScoreboardSidebar.kt
@@ -1,23 +1,74 @@
 package eu.mshade.enderframe.scoreboard
 
+import eu.mshade.enderframe.Agent
 import eu.mshade.enderframe.entity.Entity
 import eu.mshade.enderframe.entity.Player
 import eu.mshade.enderframe.mojang.chat.TextComponent
-import java.util.UUID
+import eu.mshade.enderframe.scoreboard.objective.ScoreboardObjectiveAction
+import java.util.*
 
-class ScoreboardSidebar(title: TextComponent): Scoreboard(title, ScoreboardPosition.SIDEBAR, ScoreboardType.INTEGER) {
+const val MAX_LINES = 16
+
+class ScoreboardSidebar(title: TextComponent) : Scoreboard(title, ScoreboardPosition.SIDEBAR, ScoreboardType.INTEGER) {
 
     private val lines = mutableListOf<ScoreboardLine>()
 
-    fun updateLine(index: Int, entry: String) {
-        val line = lines[index]
+    fun updateLine(l: Int, entry: String) {
+        if (l > MAX_LINES) throw IllegalArgumentException("Index must be smaller than $MAX_LINES")
+
+        if (lines.size <= l) {
+            for (i in lines.size until l) {
+                lines.add(DefaultScoreboardLine(this,"", i))
+            }
+            lines.add(DefaultScoreboardLine(this, entry, l))
+            notify(Player::class.java) {
+                val minecraftSession = it.minecraftSession
+                minecraftSession.sendUpdateScoreboardLine(lines[l], ScoreboardObjectiveAction.CHANGE)
+            }
+            return
+        }
+
+        val line = lines[l]
+        notify(Player::class.java) {
+            val minecraftSession = it.minecraftSession
+            minecraftSession.sendUpdateScoreboardLine(line, ScoreboardObjectiveAction.REMOVE)
+        }
         line.setEntry(entry)
         notify(Player::class.java) {
             val minecraftSession = it.minecraftSession
-
+            minecraftSession.sendUpdateScoreboardLine(line, ScoreboardObjectiveAction.CHANGE)
         }
     }
 
+    fun updateLine(vararg entries: String) {
+        if (entries.size > MAX_LINES) throw IllegalArgumentException("Index must be smaller than $MAX_LINES")
+        for (i in entries.indices) {
+            updateLine(i, entries[i])
+        }
+    }
+
+    fun deleteLine(l: Int) {
+        if (l > MAX_LINES) throw IllegalArgumentException("Index must be smaller than $MAX_LINES")
+        val line = lines[l]
+        notify(Player::class.java) {
+            val minecraftSession = it.minecraftSession
+            minecraftSession.sendUpdateScoreboardLine(line, ScoreboardObjectiveAction.REMOVE)
+        }
+        lines.removeAt(l)
+    }
+
+    override fun addWatcher(agent: Agent) {
+        val add = viewers.add(agent)
+        if (add) {
+            val player = agent as? Player ?: return
+            val minecraftSession = player.minecraftSession
+            minecraftSession.sendScoreboard(this, ScoreboardMode.CREATE)
+            minecraftSession.sendDisplayScoreboard(this)
+            for (line in lines) {
+                minecraftSession.sendUpdateScoreboardLine(line, ScoreboardObjectiveAction.CHANGE)
+            }
+        }
+    }
 }
 
 interface ScoreboardLine {
@@ -34,32 +85,33 @@ interface ScoreboardLine {
 
 }
 
-interface EntityScoreboardLine: ScoreboardLine {
+interface EntityScoreboardLine : ScoreboardLine {
 
     fun getEntity(): Entity
 
 }
 
-class DefaultScoreboardLine(val uniqueId: String = UUID.randomUUID().toString(), var entry: String, var score: Int): ScoreboardLine {
+class DefaultScoreboardLine(private val scoreboard: Scoreboard, private var entry: String, private var score: Int, private val uniqueId: String = UUID.randomUUID().toString()) :
+    ScoreboardLine {
 
     override fun getEntry(): String {
-        TODO("Not yet implemented")
+        return entry
     }
 
     override fun setEntry(entry: String) {
-        TODO("Not yet implemented")
+        this.entry = entry
     }
 
     override fun getScore(): Int {
-        TODO("Not yet implemented")
+        return score
     }
 
     override fun setScore(score: Int) {
-        TODO("Not yet implemented")
+        this.score = score
     }
 
     override fun getScoreboard(): Scoreboard {
-        TODO("Not yet implemented")
+        return scoreboard
     }
 
 
